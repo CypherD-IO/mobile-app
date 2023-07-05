d9e689a0e5495f042d673e71409b5f56180d83ed
shan-c <114131317+shan-c@users.noreply.github.com>
2023-07-05 23:37:14 +0530

    Added decodeURIComponent for walletconnect v2.0 intent URL (#11)

--- src/components/walletConnectV2Provider/index.tsx
+++ src/components/walletConnectV2Provider/index.tsx
@@ -1,6 +1,6 @@
 import WalletConnect from '@walletconnect/client';
 import React, { createContext, useContext, useEffect } from 'react';
-import { PortfolioContext } from '../../core/util';
+import { HdWalletContext, PortfolioContext } from '../../core/util';
 import { web3WalletPair } from '../../core/walletConnectV2Utils';
 import useInitialIntentURL from '../../hooks/useInitialIntentURL';
 import useWalletConnectEventsManager from '../../hooks/useWalletConnectV2EventsManager';
@@ -17,6 +17,8 @@ export const WalletConnectV2Provider: React.FC<any> = ({ children }) => {
   // Step 1 - Initialize wallets and wallet connect client
   const initialized = useWalletConnectV2Initialization();
   const portfolioState = useContext<any>(PortfolioContext);
+  const hdWalletContext = useContext<any>(HdWalletContext);
+  const ethereum = hdWalletContext.state.wallet.ethereum;
   const { walletConnectDispatch } = useContext<any>(WalletConnectContext);
 
   // Step 2 - Once initialized, set up wallet connect event manager
@@ -26,16 +28,16 @@ export const WalletConnectV2Provider: React.FC<any> = ({ children }) => {
 
   const initiateWalletConnection = async () => {
     if (initialUrl) {
-      let uri = '';
-      if (initialUrl?.url) {
-        uri = initialUrl?.url?.replace('cypherwallet://', '');
-      } else if (initialUrl?.includes('cypherwallet://')) {
-        uri = initialUrl?.replace('cypherwallet://', '');
+      let uri = initialUrl?.url ?? initialUrl;
+      if (uri?.includes('cypherwallet://')) {
+        uri = uri?.replace('cypherwallet://', '');
       }
-      console.log(uri);
+      if (uri?.includes('wc?uri')) {
+        uri = uri?.replace('wc?uri=', '');
+      }
       if (uri?.startsWith('wc') && (uri.includes('bridge') || uri.includes('relay-protocol'))) {
         if (uri.includes('relay-protocol')) {
-          await web3WalletPair({ uri });
+          await web3WalletPair({ uri: decodeURIComponent(uri) });
         } else if (uri.includes('bridge')) {
           const connector = new WalletConnect({ uri });
           portfolioState.dispatchPortfolio({ value: { walletConnectURI: uri } });
@@ -46,10 +48,10 @@ export const WalletConnectV2Provider: React.FC<any> = ({ children }) => {
   };
 
   useEffect(() => {
-    if (initialized) {
+    if (initialized && ethereum?.wallets[ethereum?.currentIndex]?.address) {
       void initiateWalletConnection();
     }
-  }, [initialUrl, initialized]);
+  }, [initialUrl, initialized, ethereum.wallets.length]);
 
   return (
     <WalletConnectContext.Provider value={{ initialized }}>
