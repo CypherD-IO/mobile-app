4eb95bf8120ef9d5e12d216f6b206d0135124bc9
karthickcypher <karthick@cypherd.io>
2023-07-21 11:49:12 +0530

    change: modal with position absolute

--- src/components/MoreViewModal.tsx
+++ src/components/MoreViewModal.tsx
@@ -16,19 +16,19 @@ export default function MoreViewModal (props: any) {
       animationIn={'fadeIn'}
       animationOut={'fadeOut'}
     >
-      <CyDView className='flex flex-col justify-start items-end pointer-events-none w-[250px] h-[500px] ml-[80px]'>
-          <CyDView className='rounded-[10px] flex items-start justify-start bg-secondaryBackgroundColor p-[15px]'>
-              <CyDTouchView sentry-label='more-view-home-icon' className='flex flex-row justify-start items-center gap-[16px] my-[1px] ' onPress={() => { onHome(); onPress(); }}>
+      <CyDView className='pointer-events-none w-fit h-full relative'>
+          <CyDView className='bg-secondaryBackgroundColor rounded-[10px] p-[15px] absolute top-[60px] right-[0px]'>
+              <CyDTouchView sentry-label='more-view-home-icon' className='flex flex-row justify-start items-center gap-[16px] my-[1px]' onPress={() => { onHome(); onPress(); }}>
                 <CyDFastImage source={AppImages.HOME_BROWSER} className='h-[22px] w-[22px]' resizeMode='contain' />
-                <CyDText className='text-[16px] font-semibold text-secondaryTextColor'>{t('HOME')}</CyDText>
+                <CyDText className='text-[16px] font-medium text-secondaryTextColor'>{t('HOME')}</CyDText>
               </CyDTouchView>
-              <CyDTouchView sentry-label='more-view-history-icon' className='flex flex-row justify-start items-center gap-[16px] my-[1px] ' onPress={() => { onHistory(); onPress(); }}>
+              <CyDTouchView sentry-label='more-view-history-icon' className='flex flex-row justify-start items-center gap-[16px] my-[1px]' onPress={() => { onHistory(); onPress(); }}>
                 <CyDFastImage source={AppImages.HISTORY_BROWSER} className='h-[22px] w-[22px]' resizeMode='contain' />
-                <CyDText className='text-[16px] font-semibold text-secondaryTextColor'>{t('HISTORY')}</CyDText>
+                <CyDText className='text-[16px] font-medium text-secondaryTextColor'>{t('HISTORY')}</CyDText>
               </CyDTouchView>
-              <CyDTouchView sentry-label='more-view-bookmark-icon' className='flex flex-row justify-start items-center gap-[16px] my-[1px] ' onPress={() => { onBookmark(); onPress(); }}>
+              <CyDTouchView sentry-label='more-view-bookmark-icon' className='flex flex-row justify-start items-center gap-[16px] my-[1px]' onPress={() => { onBookmark(); onPress(); }}>
                 <CyDFastImage source={AppImages.BOOKMARK_BROWSER} className='h-[22px] w-[22px]' resizeMode='contain' />
-                <CyDText className='text-[16px] font-semibold text-secondaryTextColor'>{t('BROWSER')}</CyDText>
+                <CyDText className='text-[16px] font-medium text-secondaryTextColor'>{t('BROWSER')}</CyDText>
               </CyDTouchView>
           </CyDView>
       </CyDView>
