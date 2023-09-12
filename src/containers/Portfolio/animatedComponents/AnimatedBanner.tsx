366c82bb793e102dbc28eadbc5ea578baa462558
karthickcypher <karthick@cypherd.io>
2023-09-12 12:36:37 +0530

    change: AnimatedBanner - manual safeInset

--- src/containers/Portfolio/animatedComponents/AnimatedBanner.tsx
+++ src/containers/Portfolio/animatedComponents/AnimatedBanner.tsx
@@ -1,16 +1,13 @@
 import React from 'react';
-import {
-  CyDAnimatedView,
-  CyDSafeAreaView,
-} from '../../../styles/tailwindStyles';
-import { H_BALANCE_BANNER, OFFSET_TABVIEW } from '../constants';
+import { CyDAnimatedView } from '../../../styles/tailwindStyles';
+import { H_BALANCE_BANNER, H_TAB_BAR, OFFSET_TABVIEW } from '../constants';
 import {
   Extrapolate,
   SharedValue,
   interpolate,
   useAnimatedStyle,
 } from 'react-native-reanimated';
-
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 export interface AnimatedBannerProps {
   scrollY: SharedValue<number>;
   children: JSX.Element;
@@ -21,18 +18,19 @@ export const AnimatedBanner = ({
   children,
   ...otherProps
 }: AnimatedBannerProps) => {
+  const topInset = useSafeAreaInsets().top + H_TAB_BAR;
   const animatedStyles = useAnimatedStyle(() => {
     const translateY = interpolate(
       scrollY.value,
       [OFFSET_TABVIEW, OFFSET_TABVIEW + H_BALANCE_BANNER],
       [0, -H_BALANCE_BANNER],
-      Extrapolate.CLAMP
+      Extrapolate.CLAMP,
     );
     const opacity = interpolate(
       scrollY.value,
       [OFFSET_TABVIEW, OFFSET_TABVIEW + 0.6 * H_BALANCE_BANNER],
       [1, 0],
-      Extrapolate.CLAMP
+      Extrapolate.CLAMP,
     );
     return {
       opacity,
@@ -40,14 +38,12 @@ export const AnimatedBanner = ({
     };
   });
   return (
-    <CyDSafeAreaView className='z-10 mx-[10px]'>
-      <CyDAnimatedView
-        className={`absolute top-0 h-[${H_BALANCE_BANNER}px] w-full bg-white`}
-        style={animatedStyles}
-        {...otherProps}
-      >
-        {children}
-      </CyDAnimatedView>
-    </CyDSafeAreaView>
+    <CyDAnimatedView
+      className={`absolute top-[${topInset}px] h-[${H_BALANCE_BANNER}px] z-10 px-[10px] w-full bg-white`}
+      style={animatedStyles}
+      {...otherProps}
+    >
+      {children}
+    </CyDAnimatedView>
   );
 };
