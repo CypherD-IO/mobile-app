9556ffa754f2ad24dd16df9f56b8488b4e67c25a
shan-c <shanthan@cypherd.io>
2023-09-30 02:03:07 +0530

    Added gas less transaction for noble usdc for send and IBC

--- src/constants/cosmosConfig.ts
+++ src/constants/cosmosConfig.ts
@@ -6,18 +6,18 @@ export enum Denom {
   EVMOS = 'aevmos',
   JUNO = 'ujuno',
   STARGAZE = 'ustars',
-  NOBLE = 'uatom'
+  NOBLE = 'uusdc',
 }
 
 export interface IIBCData {
-  prefix: string
-  denom: Denom
-  contractDecimal: number
-  coinType: number
-  channel: Record<string, string>
-  gasPrice: number
-  ibcDenoms: Record<string, string>
-  poolIds?: Record<string, Long>
+  prefix: string;
+  denom: Denom;
+  contractDecimal: number;
+  coinType: number;
+  channel: Record<string, string>;
+  gasPrice: number;
+  ibcDenoms: Record<string, string>;
+  poolIds?: Record<string, Long>;
 }
 
 export const cosmosConfig: Record<string, IIBCData> = {
@@ -31,17 +31,21 @@ export const cosmosConfig: Record<string, IIBCData> = {
       evmos: 'channel-292',
       juno: 'channel-207',
       stargaze: 'channel-285',
-      noble: 'channel-536'
+      noble: 'channel-536',
     },
     gasPrice: 0.000000012898297,
     ibcDenoms: {
       cosmos: 'uatom',
-      osmosis: 'ibc/14F9BC3E44B8A9C1BE1FB08980FAB87034C9905EF17CF2F5008FC085218811CC',
-      evmos: 'ibc/19DD710119533524061885A6F190B18AF28D9537E2BAE37F32A62C1A25979287',
+      osmosis:
+        'ibc/14F9BC3E44B8A9C1BE1FB08980FAB87034C9905EF17CF2F5008FC085218811CC',
+      evmos:
+        'ibc/19DD710119533524061885A6F190B18AF28D9537E2BAE37F32A62C1A25979287',
       juno: 'ibc/CDAB23DA5495290063363BD1C3499E26189036302DC689985A7E23F8DF8D8DB0',
-      stargaze: 'ibc/902EB27DB5573282C1200C0E681541C1D4176CF851811530A5B77140777B0769',
-      noble: 'ibc/EF48E6B1A1A19F47ECAEA62F5670C37C0580E86A9E88498B7E393EB6F49F33C0'
-    }
+      stargaze:
+        'ibc/902EB27DB5573282C1200C0E681541C1D4176CF851811530A5B77140777B0769',
+      noble:
+        'ibc/EF48E6B1A1A19F47ECAEA62F5670C37C0580E86A9E88498B7E393EB6F49F33C0',
+    },
   },
   osmosis: {
     prefix: 'osmo',
@@ -53,24 +57,27 @@ export const cosmosConfig: Record<string, IIBCData> = {
       evmos: 'channel-204',
       juno: 'channel-42',
       stargaze: 'channel-75',
-      noble: 'channel-750'
+      noble: 'channel-750',
     },
     gasPrice: 0.000000040006986,
     ibcDenoms: {
       osmosis: 'uosmo',
-      cosmos: 'ibc/27394FB092D2ECCD56123C74F36E4C1F926001CEADA9CA97EA622B25F41E5EB2',
-      evmos: 'ibc/6AE98883D4D5D5FF9E50D7130F1305DA2FFA0C652D1DD9C123657C6B4EB2DF8A',
+      cosmos:
+        'ibc/27394FB092D2ECCD56123C74F36E4C1F926001CEADA9CA97EA622B25F41E5EB2',
+      evmos:
+        'ibc/6AE98883D4D5D5FF9E50D7130F1305DA2FFA0C652D1DD9C123657C6B4EB2DF8A',
       juno: 'ibc/46B44899322F3CD854D2D46DEEF881958467CDD4B3B10086DA49296BBED94BED',
-      stargaze: 'ibc/987C17B11ABC2B20019178ACE62929FE9840202CE79498E29FE8E5CB02B7C0A4',
-      noble: ''
+      stargaze:
+        'ibc/987C17B11ABC2B20019178ACE62929FE9840202CE79498E29FE8E5CB02B7C0A4',
+      noble: '',
     },
     poolIds: {
       cosmos: Long.fromNumber(1),
       evmos: Long.fromNumber(722),
       juno: Long.fromNumber(497),
       stargaze: Long.fromNumber(604),
-      noble: Long.fromNumber(605)
-    }
+      noble: Long.fromNumber(605),
+    },
   },
   evmos: {
     prefix: 'evmos',
@@ -82,16 +89,19 @@ export const cosmosConfig: Record<string, IIBCData> = {
       osmosis: 'channel-0',
       juno: 'channel-5',
       stargaze: 'channel-13',
-      noble: 'channel-63'
+      noble: 'channel-63',
     },
     gasPrice: 0.000000075,
     ibcDenoms: {
-      cosmos: 'ibc/A4DB47A9D3CF9A068D454513891B526702455D3EF08FB9EB558C561F9DC2B701',
-      osmosis: 'ibc/ED07A3391A112B175915CD8FAF43A2DA8E4790EDE12566649D0C2F97716B8518',
+      cosmos:
+        'ibc/A4DB47A9D3CF9A068D454513891B526702455D3EF08FB9EB558C561F9DC2B701',
+      osmosis:
+        'ibc/ED07A3391A112B175915CD8FAF43A2DA8E4790EDE12566649D0C2F97716B8518',
       juno: 'ibc/448C1061CE97D86CC5E86374CD914870FB8EBA16C58661B5F1D3F46729A2422D',
-      stargaze: 'ibc/7564B7F838579DD4517A225978C623504F852A6D0FF7984AFB28F10D36022BE8',
-      noble: ''
-    }
+      stargaze:
+        'ibc/7564B7F838579DD4517A225978C623504F852A6D0FF7984AFB28F10D36022BE8',
+      noble: '',
+    },
   },
   juno: {
     prefix: 'juno',
@@ -103,17 +113,21 @@ export const cosmosConfig: Record<string, IIBCData> = {
       osmosis: 'channel-0',
       evmos: 'channel-70',
       stargaze: 'channel-20',
-      noble: 'channel-224'
+      noble: 'channel-224',
     },
     gasPrice: 0.000000175,
     ibcDenoms: {
-      cosmos: 'ibc/C4CFF46FD6DE35CA4CF4CE031E643C8FDC9BA4B99AE598E9B0ED98FE3A2319F9 ',
-      osmosis: 'ibc/ED07A3391A112B175915CD8FAF43A2DA8E4790EDE12566649D0C2F97716B8518 ',
+      cosmos:
+        'ibc/C4CFF46FD6DE35CA4CF4CE031E643C8FDC9BA4B99AE598E9B0ED98FE3A2319F9 ',
+      osmosis:
+        'ibc/ED07A3391A112B175915CD8FAF43A2DA8E4790EDE12566649D0C2F97716B8518 ',
       juno: 'ujuno',
-      evmos: 'ibc/9B990F95D85E7CA8C46544975776CAA20A3DEE3507EEA829A4000D8D65617F6D',
-      stargaze: 'ibc/F6B367385300865F654E110976B838502504231705BAC0849B0651C226385885',
-      noble: ''
-    }
+      evmos:
+        'ibc/9B990F95D85E7CA8C46544975776CAA20A3DEE3507EEA829A4000D8D65617F6D',
+      stargaze:
+        'ibc/F6B367385300865F654E110976B838502504231705BAC0849B0651C226385885',
+      noble: '',
+    },
   },
   stargaze: {
     prefix: 'stars',
@@ -125,17 +139,20 @@ export const cosmosConfig: Record<string, IIBCData> = {
       evmos: 'channel-46',
       osmosis: 'channel-0',
       juno: 'channel-5',
-      noble: ''
+      noble: '',
     },
     gasPrice: 0.00001105,
     ibcDenoms: {
-      cosmos: 'ibc/6CDD4663F2F09CD62285E2D45891FC149A3568E316CE3EBBE201A71A78A69388',
-      osmosis: 'ibc/ED07A3391A112B175915CD8FAF43A2DA8E4790EDE12566649D0C2F97716B8518',
+      cosmos:
+        'ibc/6CDD4663F2F09CD62285E2D45891FC149A3568E316CE3EBBE201A71A78A69388',
+      osmosis:
+        'ibc/ED07A3391A112B175915CD8FAF43A2DA8E4790EDE12566649D0C2F97716B8518',
       juno: 'ibc/448C1061CE97D86CC5E86374CD914870FB8EBA16C58661B5F1D3F46729A2422D',
-      evmos: 'ibc/F9C792DF71F960BB9EF698493B61E29C1EBB8FCD56B1F8BB08C86871F5F497C0',
+      evmos:
+        'ibc/F9C792DF71F960BB9EF698493B61E29C1EBB8FCD56B1F8BB08C86871F5F497C0',
       stargaze: 'ustars',
-      noble: ''
-    }
+      noble: '',
+    },
   },
   noble: {
     prefix: 'noble',
@@ -147,16 +164,20 @@ export const cosmosConfig: Record<string, IIBCData> = {
       evmos: 'channel-6',
       osmosis: 'channel-1',
       juno: 'channel-3',
-      stargaze: ''
+      stargaze: '',
     },
     gasPrice: 0.00000005,
     ibcDenoms: {
-      cosmos: 'ibc/6CDD4663F2F09CD62285E2D45891FC149A3568E316CE3EBBE201A71A78A69388',
-      osmosis: 'ibc/ED07A3391A112B175915CD8FAF43A2DA8E4790EDE12566649D0C2F97716B8518',
+      cosmos:
+        'ibc/6CDD4663F2F09CD62285E2D45891FC149A3568E316CE3EBBE201A71A78A69388',
+      osmosis:
+        'ibc/ED07A3391A112B175915CD8FAF43A2DA8E4790EDE12566649D0C2F97716B8518',
       juno: 'ibc/448C1061CE97D86CC5E86374CD914870FB8EBA16C58661B5F1D3F46729A2422D',
-      evmos: 'ibc/F9C792DF71F960BB9EF698493B61E29C1EBB8FCD56B1F8BB08C86871F5F497C0',
-      stargaze: 'ibc/F6B367385300865F654E110976B838502504231705BAC0849B0651C226385885',
-      noble: 'uatom'
-    }
-  }
+      evmos:
+        'ibc/F9C792DF71F960BB9EF698493B61E29C1EBB8FCD56B1F8BB08C86871F5F497C0',
+      stargaze:
+        'ibc/F6B367385300865F654E110976B838502504231705BAC0849B0651C226385885',
+      noble: 'uatom',
+    },
+  },
 };
--- src/containers/SendTo/enterAmount.tsx
+++ src/containers/SendTo/enterAmount.tsx
@@ -11,21 +11,34 @@ import { useGlobalModalContext } from '../../components/v2/GlobalModal';
 import { BuyOrBridge } from '../../components/v2/StateModal';
 import { gasFeeReservation } from '../../constants/data';
 import * as C from '../../constants/index';
-import { ChainNameMapping, NativeTokenMapping } from '../../constants/server';
+import {
+  ChainNameMapping,
+  GASLESS_CHAINS,
+  NativeTokenMapping,
+} from '../../constants/server';
 import { Colors } from '../../constants/theme';
 import { Holding } from '../../core/Portfolio';
-import { getNativeTokenBalance, PortfolioContext, limitDecimalPlaces } from '../../core/util';
-import { CyDFastImage, CyDImage, CyDSafeAreaView, CyDScrollView, CyDText, CyDTextInput, CyDTouchView, CyDView } from '../../styles/tailwindStyles';
+import {
+  getNativeTokenBalance,
+  PortfolioContext,
+  limitDecimalPlaces,
+} from '../../core/util';
+import {
+  CyDFastImage,
+  CyDImage,
+  CyDSafeAreaView,
+  CyDScrollView,
+  CyDText,
+  CyDTextInput,
+  CyDTouchView,
+  CyDView,
+} from '../../styles/tailwindStyles';
 import CyDTokenAmount from '../../components/v2/tokenAmount';
 import CyDTokenValue from '../../components/v2/tokenValue';
 import Button from '../../components/v2/button';
 import { ButtonType } from '../../constants/enum';
 
-const {
-  CText,
-  DynamicView,
-  DynamicImage
-} = require('../../styles');
+const { CText, DynamicView, DynamicImage } = require('../../styles');
 
 export default function EnterAmount(props) {
   // NOTE: DEFINE VARIABLE 🍎🍎🍎🍎🍎🍎
@@ -39,7 +52,7 @@ export default function EnterAmount(props) {
   const [enterCryptoAmount, setEnterCryptoAmount] = useState<boolean>(true);
   const currencyFormatter = new Intl.NumberFormat('en-US', {
     style: 'currency',
-    currency: 'USD'
+    currency: 'USD',
   });
   const portfolioState = useContext<any>(PortfolioContext);
 
@@ -59,66 +72,102 @@ export default function EnterAmount(props) {
 
   const formatAmount = (amount: string) => {
     if (amount.includes('.')) {
-      return amount.slice(0, amount.indexOf('.') + (tokenData.name === 'Ether' ? 7 : 3));
+      return amount.slice(
+        0,
+        amount.indexOf('.') + (tokenData.name === 'Ether' ? 7 : 3),
+      );
     } else if (amount === '0') return '0.00';
     return amount;
   };
 
   const renderModalBody = (text: string) => {
-    return <BuyOrBridge
-      text={text}
-      navigation={props.navigation}
-      portfolioState={portfolioState}
-      hideModal={hideModal}
-    />;
+    return (
+      <BuyOrBridge
+        text={text}
+        navigation={props.navigation}
+        portfolioState={portfolioState}
+        hideModal={hideModal}
+      />
+    );
   };
 
   const isGasReservedForNative = (cryptoValue: string) => {
-    const nativeTokenSymbol = NativeTokenMapping[tokenData.chainDetails.symbol] || tokenData.chainDetails.symbol;
+    const nativeTokenSymbol =
+      NativeTokenMapping[tokenData.chainDetails.symbol] ||
+      tokenData.chainDetails.symbol;
     const isNative = tokenData.symbol === nativeTokenSymbol;
     if (!isNative) return true;
-    const gasReserved = isNative ? gasFeeReservation[tokenData.chainDetails?.backendName] : 0;
-    return parseFloat((parseFloat(tokenData.actualBalance) - gasReserved).toFixed(6)) >= parseFloat(cryptoValue);
+    const gasReserved = isNative
+      ? gasFeeReservation[tokenData.chainDetails?.backendName]
+      : 0;
+    return (
+      parseFloat(
+        (parseFloat(tokenData.actualBalance) - gasReserved).toFixed(6),
+      ) >= parseFloat(cryptoValue)
+    );
   };
 
   const haveEnoughNativeBalance = (cryptoValue: string) => {
-    const nativeBackendName = tokenData.chainDetails.backendName;
-    const nativeTokenSymbol = NativeTokenMapping[tokenData.chainDetails.symbol] || tokenData.chainDetails.symbol;
-    const nativeTokenBalance = getNativeTokenBalance(nativeTokenSymbol, portfolioState.statePortfolio.tokenPortfolio[
-      ChainNameMapping[nativeBackendName]
-    ].holdings);
-    const gasReserved = gasFeeReservation[tokenData.chainDetails?.backendName];
+    const { backendName, symbol } = tokenData?.chainDetails;
+    if (GASLESS_CHAINS.includes(backendName)) {
+      return false;
+    }
+    const nativeBackendName = backendName;
+    const nativeTokenSymbol = NativeTokenMapping[symbol] || symbol;
+    const nativeTokenBalance = getNativeTokenBalance(
+      nativeTokenSymbol,
+      portfolioState.statePortfolio.tokenPortfolio[
+        ChainNameMapping[nativeBackendName]
+      ].holdings,
+    );
+    const gasReserved = gasFeeReservation[backendName];
 
     return nativeTokenBalance <= gasReserved;
   };
 
   const _validateValueForUsd = () => {
-    const nativeTokenSymbol = NativeTokenMapping[tokenData.chainDetails.symbol] || tokenData.chainDetails.symbol;
+    const nativeTokenSymbol =
+      NativeTokenMapping[tokenData.chainDetails.symbol] ||
+      tokenData.chainDetails.symbol;
     if (parseFloat(cryptoValue) > parseFloat(tokenData.actualBalance)) {
-      showModal('state', { type: 'error', title: t('INSUFFICIENT_FUNDS'), description: t('ENTER_AMOUNT_LESS_THAN_BALANCE'), onSuccess: hideModal, onFailure: hideModal });
+      showModal('state', {
+        type: 'error',
+        title: t('INSUFFICIENT_FUNDS'),
+        description: t('ENTER_AMOUNT_LESS_THAN_BALANCE'),
+        onSuccess: hideModal,
+        onFailure: hideModal,
+      });
     } else if (haveEnoughNativeBalance(cryptoValue)) {
       showModal('state', {
         type: 'error',
         title: t('INSUFFICIENT_FUNDS'),
-        description: renderModalBody(`You don't have sufficient ${nativeTokenSymbol} to pay gas fee. Would you like to buy or bridge?`),
+        description: renderModalBody(
+          `You don't have sufficient ${nativeTokenSymbol} to pay gas fee. Would you like to buy or bridge?`,
+        ),
         onSuccess: hideModal,
-        onFailure: hideModal
+        onFailure: hideModal,
       });
     } else if (!isGasReservedForNative(cryptoValue)) {
-      const cryVal = parseFloat(tokenData.actualBalance) - gasFeeReservation[tokenData.chainDetails.backendName];
-      const reqAmount = enterCryptoAmount ? `${cryVal.toFixed(6)} ${tokenData.symbol}` : `${parseFloat((cryVal * parseFloat(tokenData.price)).toFixed(6))} USD`;
+      const cryVal =
+        parseFloat(tokenData.actualBalance) -
+        gasFeeReservation[tokenData.chainDetails.backendName];
+      const reqAmount = enterCryptoAmount
+        ? `${cryVal.toFixed(6)} ${tokenData.symbol}`
+        : `${parseFloat(
+            (cryVal * parseFloat(tokenData.price)).toFixed(6),
+          )} USD`;
       showModal('state', {
         type: 'error',
         title: t('INSUFFICIENT_GAS_FEE'),
         description: `Enter amount lesser than ${reqAmount}`,
         onSuccess: hideModal,
-        onFailure: hideModal
+        onFailure: hideModal,
       });
     } else {
       props.navigation.navigate(C.screenTitle.SEND_TO, {
         valueForUsd: cryptoValue,
         tokenData,
-        sendAddress
+        sendAddress,
       });
     }
   };
@@ -128,35 +177,55 @@ export default function EnterAmount(props) {
     <CyDSafeAreaView className='flex-1 bg-white'>
       <CyDView className={'bg-white w-full'}>
         <CyDView>
-          <CyDView className={'flex items-center justify-center pb-[35px] pt-[15px] w-full bg-secondaryBackgroundColor rounded-b-[25px]'}>
-            <CyDView className={'flex items-center justify-center w-full relative'}>
+          <CyDView
+            className={
+              'flex items-center justify-center pb-[35px] pt-[15px] w-full bg-secondaryBackgroundColor rounded-b-[25px]'
+            }
+          >
+            <CyDView
+              className={'flex items-center justify-center w-full relative'}
+            >
               <CyDTouchView
                 onPress={() => {
-                  const gasReserved = (NativeTokenMapping[tokenData?.chainDetails?.symbol] || tokenData?.chainDetails?.symbol) === tokenData?.symbol ? gasFeeReservation[tokenData.chainDetails.backendName] : 0;
+                  const gasReserved =
+                    (NativeTokenMapping[tokenData?.chainDetails?.symbol] ||
+                      tokenData?.chainDetails?.symbol) === tokenData?.symbol
+                      ? gasFeeReservation[tokenData.chainDetails.backendName]
+                      : 0;
 
-                  const maxAmount = parseFloat(tokenData?.actualBalance) - gasReserved;
-                  const textAmount = maxAmount < 0 ? '0.00' : limitDecimalPlaces(maxAmount.toString(), 6);
+                  const maxAmount =
+                    parseFloat(tokenData?.actualBalance) - gasReserved;
+                  const textAmount =
+                    maxAmount < 0
+                      ? '0.00'
+                      : limitDecimalPlaces(maxAmount.toString(), 6);
                   setValueForUsd(textAmount);
 
                   if (enterCryptoAmount) {
                     setCryptoValue(textAmount);
-                    setUsdValue((parseFloat(textAmount) * tokenData.price).toString());
+                    setUsdValue(
+                      (parseFloat(textAmount) * tokenData.price).toString(),
+                    );
                   } else {
-                    setCryptoValue((parseFloat(textAmount) / tokenData.price).toString());
+                    setCryptoValue(
+                      (parseFloat(textAmount) / tokenData.price).toString(),
+                    );
                     setUsdValue(textAmount);
                   }
                 }}
                 className={clsx(
-                  'absolute left-[10%] bottom-[60%] bg-white rounded-full h-[40px] w-[40px] flex justify-center items-center p-[4px]'
-                )
-                }
+                  'absolute left-[10%] bottom-[60%] bg-white rounded-full h-[40px] w-[40px] flex justify-center items-center p-[4px]',
+                )}
                 style={styles.roundButtonContainer}
               >
-                <CyDText className={'font-nunito text-black '} >{t<string>('MAX')}</CyDText>
+                <CyDText className={'font-nunito text-black '}>
+                  {t<string>('MAX')}
+                </CyDText>
               </CyDTouchView>
               <CyDTouchView
                 onPress={() => {
-                  if (enterCryptoAmount) setValueForUsd(limitDecimalPlaces(usdValue, 6));
+                  if (enterCryptoAmount)
+                    setValueForUsd(limitDecimalPlaces(usdValue, 6));
                   else setValueForUsd(limitDecimalPlaces(cryptoValue, 6));
                   setEnterCryptoAmount(!enterCryptoAmount);
                   // if (!enterCryptoAmount) {
@@ -168,11 +237,14 @@ export default function EnterAmount(props) {
                   // }
                 }}
                 className={clsx(
-                  'absolute right-[10%] bottom-[60%] bg-white rounded-full h-[40px] w-[40px] flex justify-center items-center p-[4px]'
+                  'absolute right-[10%] bottom-[60%] bg-white rounded-full h-[40px] w-[40px] flex justify-center items-center p-[4px]',
                 )}
                 style={styles.roundButtonContainer}
               >
-                <CyDImage source={AppImages.TOGGLE_ICON} className={'w-[14px] h-[16px]'} />
+                <CyDImage
+                  source={AppImages.TOGGLE_ICON}
+                  className={'w-[14px] h-[16px]'}
+                />
               </CyDTouchView>
               <CyDText className='font-nunito text-[15px] font-bold text-primaryTextColor'>
                 {enterCryptoAmount ? tokenData.symbol : 'USD'}
@@ -183,17 +255,21 @@ export default function EnterAmount(props) {
                     'font-bold text-center text-black h-[85px] font-nunito',
                     {
                       'text-[70px]': valueForUsd.length <= 5,
-                      'text-[40px]': valueForUsd.length > 5
-                    }
+                      'text-[40px]': valueForUsd.length > 5,
+                    },
                   )}
-                  keyboardType="numeric"
+                  keyboardType='numeric'
                   onChangeText={(text) => {
                     setValueForUsd(text);
                     if (enterCryptoAmount) {
                       setCryptoValue(text);
-                      setUsdValue((parseFloat(text) * tokenData.price).toString());
+                      setUsdValue(
+                        (parseFloat(text) * tokenData.price).toString(),
+                      );
                     } else {
-                      setCryptoValue((parseFloat(text) / tokenData.price).toString());
+                      setCryptoValue(
+                        (parseFloat(text) / tokenData.price).toString(),
+                      );
                       setUsdValue(text);
                     }
                   }}
@@ -207,14 +283,30 @@ export default function EnterAmount(props) {
                   }}
                 />
               </CyDView>
-              <CText dynamic fF={C.fontsName.FONT_BOLD} fS={15} color={Colors.subTextColor}>{enterCryptoAmount ? (!isNaN(parseFloat(usdValue)) ? formatAmount(usdValue) : '0.00') + ' USD' : (!isNaN(parseFloat(cryptoValue)) ? formatAmount(cryptoValue) : '0.00') + ` ${tokenData.name}`}</CText>
+              <CText
+                dynamic
+                fF={C.fontsName.FONT_BOLD}
+                fS={15}
+                color={Colors.subTextColor}
+              >
+                {enterCryptoAmount
+                  ? (!isNaN(parseFloat(usdValue))
+                      ? formatAmount(usdValue)
+                      : '0.00') + ' USD'
+                  : (!isNaN(parseFloat(cryptoValue))
+                      ? formatAmount(cryptoValue)
+                      : '0.00') + ` ${tokenData.name}`}
+              </CText>
 
-              <CyDView style={styles.tokenContainer} className='flex flex-row mt-[12px] mb-[6px] items-center rounded-[10px] self-center px-[10px] bg-white'>
+              <CyDView
+                style={styles.tokenContainer}
+                className='flex flex-row mt-[12px] mb-[6px] items-center rounded-[10px] self-center px-[10px] bg-white'
+              >
                 <CyDView>
                   <CyDFastImage
                     className={'h-[35px] w-[35px] rounded-[50px]'}
                     source={{
-                      uri: tokenData.logoUrl
+                      uri: tokenData.logoUrl,
                     }}
                     resizeMode='contain'
                   />
@@ -223,9 +315,17 @@ export default function EnterAmount(props) {
                   <CyDView className='flex flex-row w-full justify-between max-h-[90px] py-[10px] items-center'>
                     <CyDView className='ml-[10px] max-w-[75%]'>
                       <CyDView className={'flex flex-row align-center'}>
-                        <CyDText className={'font-extrabold text-[16px]'}>{tokenData.name}</CyDText>
+                        <CyDText className={'font-extrabold text-[16px]'}>
+                          {tokenData.name}
+                        </CyDText>
                       </CyDView>
-                      <CyDText className={'text-[14px] text-subTextColor font-bold mt-[2px]'}>{tokenData.symbol}</CyDText>
+                      <CyDText
+                        className={
+                          'text-[14px] text-subTextColor font-bold mt-[2px]'
+                        }
+                      >
+                        {tokenData.symbol}
+                      </CyDText>
                     </CyDView>
                     <CyDView className='flex self-center items-end'>
                       <CyDTokenValue className='text-[16px] font-extrabold'>
@@ -240,19 +340,31 @@ export default function EnterAmount(props) {
               </CyDView>
               <CyDView className='flex flex-row justify-center items-center mt-[20px] w-[80px]'>
                 <CyDText className='text-[15px]'>{t('SEND_ON')}</CyDText>
-                <CyDFastImage className='h-[15px] w-[15px] mt-[2px] ml-[5px]' source={tokenData.chainDetails?.logo_url} resizeMode='contain' />
-                <CyDText className='text-[15px] ml-[5px]'>{tokenData.chainDetails?.name}</CyDText>
+                <CyDFastImage
+                  className='h-[15px] w-[15px] mt-[2px] ml-[5px]'
+                  source={tokenData.chainDetails?.logo_url}
+                  resizeMode='contain'
+                />
+                <CyDText className='text-[15px] ml-[5px]'>
+                  {tokenData.chainDetails?.name}
+                </CyDText>
               </CyDView>
             </CyDView>
           </CyDView>
         </CyDView>
       </CyDView>
       <CyDView className={'w-full items-center top-[-30px]'}>
-        <Button title={t('CONTINUE')} disabled={parseFloat(valueForUsd) <= 0 || valueForUsd === ''} onPress={() => {
-          if (valueForUsd.length > 0) {
-            _validateValueForUsd();
-          }
-        }} type={ButtonType.PRIMARY} style='h-[60px] w-3/4' />
+        <Button
+          title={t('CONTINUE')}
+          disabled={parseFloat(valueForUsd) <= 0 || valueForUsd === ''}
+          onPress={() => {
+            if (valueForUsd.length > 0) {
+              _validateValueForUsd();
+            }
+          }}
+          type={ButtonType.PRIMARY}
+          style='h-[60px] w-3/4'
+        />
       </CyDView>
     </CyDSafeAreaView>
   );
@@ -264,13 +376,13 @@ const styles = StyleSheet.create({
     shadowOffset: { width: 0, height: 1 },
     shadowOpacity: 0.8,
     elevation: 5,
-    shadowRadius: 1
+    shadowRadius: 1,
   },
   roundButtonContainer: {
     shadowColor: '#E2E4F0',
     shadowOffset: { width: 0, height: 1 },
     shadowOpacity: 0.8,
     elevation: 5,
-    shadowRadius: 10
-  }
+    shadowRadius: 10,
+  },
 });
