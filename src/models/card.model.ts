07763b66e4963a416a2c38fd92b454e6d75c932d
karthickcypher <karthick@cypherd.io>
2023-10-10 15:14:14 +0530

    refactor: :sparkles: add interfaces related to CardTransaction
    Added the interfaces for beter type safety

--- src/models/card.model.ts
+++ src/models/card.model.ts
@@ -1,4 +1,5 @@
 import { CardTransactionTypes } from "../constants/enum";
+import { ChainBackendNames } from "../constants/server";
 
 export interface Card {
   bin: string
@@ -26,4 +27,58 @@ export interface CardTransaction {
       tokenNos: number,
       tokenAddress: string
   }
+}
+
+export interface ICardSubObjectMerchant {
+  merchantName: string;
+  merchantCity: string;
+  merchantState: string;
+  merchantCountry: string;
+  postalCode: string;
+  merchantCategory: string;
+  merchantCategoryCode: string;
+  merchantId: string;
+}
+
+export interface ICardSubObject {
+  wallet: string;
+  authMethod: string;
+  localTransactionAmount: string;
+  localTransactionCurrency: string;
+  currencyConversionRate?: string;
+  merchant: ICardSubObjectMerchant;
+}
+
+export interface ITokenData {
+  id: string;
+  chain: ChainBackendNames;
+  coinId: string;
+  hash: string;
+  symbol: string;
+  tokenNos: string;
+  tokenAddress: string;
+}
+
+export interface ICardTransactions {
+  id: string;
+  type: CardTransactionTypes;
+  category?: string;
+  iconUrl?: string;
+  title: string;
+  amount: number;
+  date: string;
+  fId?: string;
+  metadata?: ICardSubObject;
+  tokenData?: ITokenData;
+  closingBalance?: number;
+  fxCurrencyName?: string;
+  fxCurrencyNumber?: number;
+  fxCurrencySymbol?: string;
+  fxCurrencyValue?: number;
+  fxConversionPrice?: number;
+  cardId?: string;
+  last4?: string;
+  userId?: string;
+  label?: string;
+  isSettled?: boolean;
 }
