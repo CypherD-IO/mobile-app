37c736e5e1432a71c2245ebff4ed715e20b18754
karthickcypher <karthick@cypherd.io>
2023-10-21 12:07:45 +0530

    fix: :bug: add evmos to portfolio chain filter, change enter otp input placeholder
    When removing the cosmos chains from the Portfolio chain filter, evmos was also removed. This has been reverted. Changed the text from Enter PIN to Enter OTP for the OTPInput textinput component.

--- src/components/v2/OTPInput.tsx
+++ src/components/v2/OTPInput.tsx
@@ -16,7 +16,7 @@ interface InputProps {
   placeholder?: string
 }
 
-function OtpInput ({ pinCount, getOtp, showButton = false, buttonCTA = t('SUBMIT'), showSecuredEntryToggle = false, loader = false, placeholder = t('ENTER_PIN_PLACEHOLDER') }: InputProps) {
+function OtpInput({ pinCount, getOtp, showButton = false, buttonCTA = t('SUBMIT'), showSecuredEntryToggle = false, loader = false, placeholder = t('ENTER_OTP') }: InputProps) {
   const [otp, setOTP] = useState<string>('');
   const [securedEntry, setSecuredEntry] = useState<boolean>(true);
   const [isLoading, setIsLoading] = useState(false);
@@ -37,35 +37,35 @@ function OtpInput ({ pinCount, getOtp, showButton = false, buttonCTA = t('SUBMIT
   };
 
   return (
-        <CyDView>
-          <CyDView className={'flex flex-row justify-between items-center rounded-[5px] border-[1px] border-inputBorderColor'}>
-              <CyDTextInput
-                  className={clsx('h-[55px] text-center w-[100%] tracking-[5px]', { 'pl-[1px] pt-[2px]': isAndroid(), 'tracking-[15px]': otp !== '', 'w-[90%] pl-[35px]': showSecuredEntryToggle })}
-                  keyboardType="numeric"
-                  placeholder={placeholder}
-                  placeholderTextColor={'#C5C5C5'}
-                  secureTextEntry={showSecuredEntryToggle && securedEntry}
-                  onChangeText={num => setOTP(num)}
-                  value={otp}
-                  maxLength={pinCount}
-              />
-              { showSecuredEntryToggle && <CyDView className={'items-end'}>
-                { securedEntry && <CyDTouchView onPress={() => { toggleSecuredEntry(); }}><CyDImage source={AppImages.EYE_OPEN} className={'w-[27px] h-[18px] mr-[12px]'} /></CyDTouchView> }
-                { !securedEntry && <CyDTouchView onPress={() => { toggleSecuredEntry(); }}><CyDImage source={AppImages.EYE_CLOSE} className={'w-[27px] h-[20px] mr-[12px]'} /></CyDTouchView> }
-              </CyDView> }
-          </CyDView>
-          {showButton && <CyDView>
-            <Button
-              disabled={otp?.length !== pinCount}
-              onPress={() => { setIsLoading(loader); getOtp(otp); }}
-              title={buttonCTA ?? ''}
-              loading={isLoading}
-              isLottie={false}
-              style={'bg-appColor py-[20px] flex flex-row justify-center items-center rounded-[12px] w-[70%] mx-auto mt-[60px]'}
-              loaderStyle={{ height: 21 }}
-            />
-          </CyDView>}
-        </CyDView>
+    <CyDView>
+      <CyDView className={'flex flex-row justify-between items-center rounded-[5px] border-[1px] border-inputBorderColor'}>
+        <CyDTextInput
+          className={clsx('h-[55px] text-center w-[100%] tracking-[5px]', { 'pl-[1px] pt-[2px]': isAndroid(), 'tracking-[15px]': otp !== '', 'w-[90%] pl-[35px]': showSecuredEntryToggle })}
+          keyboardType="numeric"
+          placeholder={placeholder}
+          placeholderTextColor={'#C5C5C5'}
+          secureTextEntry={showSecuredEntryToggle && securedEntry}
+          onChangeText={num => setOTP(num)}
+          value={otp}
+          maxLength={pinCount}
+        />
+        {showSecuredEntryToggle && <CyDView className={'items-end'}>
+          {securedEntry && <CyDTouchView onPress={() => { toggleSecuredEntry(); }}><CyDImage source={AppImages.EYE_OPEN} className={'w-[27px] h-[18px] mr-[12px]'} /></CyDTouchView>}
+          {!securedEntry && <CyDTouchView onPress={() => { toggleSecuredEntry(); }}><CyDImage source={AppImages.EYE_CLOSE} className={'w-[27px] h-[20px] mr-[12px]'} /></CyDTouchView>}
+        </CyDView>}
+      </CyDView>
+      {showButton && <CyDView>
+        <Button
+          disabled={otp?.length !== pinCount}
+          onPress={() => { setIsLoading(loader); getOtp(otp); }}
+          title={buttonCTA ?? ''}
+          loading={isLoading}
+          isLottie={false}
+          style={'bg-appColor py-[20px] flex flex-row justify-center items-center rounded-[12px] w-[70%] mx-auto mt-[60px]'}
+          loaderStyle={{ height: 21 }}
+        />
+      </CyDView>}
+    </CyDView>
   );
 }
 export default OtpInput;
