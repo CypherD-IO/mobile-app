df638ccdbac678a14601aa96a0002a37c27819fb
Karthickpranav S N <132334547+karthickcypher@users.noreply.github.com>
2023-10-31 15:03:15 +0530

    fix: :adhesive_bandage: added context aware placeholders for OtpInput (#157)
    Checked all the instances of OtpInput component usage and made the corrections to use the correct placeholder text.

--- src/containers/DebitCard/bridgeCard/activateCard.tsx
+++ src/containers/DebitCard/bridgeCard/activateCard.tsx
@@ -1,5 +1,12 @@
 import React, { useContext, useEffect, useState } from 'react';
-import { CyDSafeAreaView, CyDScrollView, CyDText, CyDTextInput, CyDTouchView, CyDView } from '../../../styles/tailwindStyles';
+import {
+  CyDSafeAreaView,
+  CyDScrollView,
+  CyDText,
+  CyDTextInput,
+  CyDTouchView,
+  CyDView,
+} from '../../../styles/tailwindStyles';
 import { useTranslation } from 'react-i18next';
 import OtpInput from '../../../components/v2/OTPInput';
 import AppImages from '../../../../assets/images/appImages';
@@ -8,7 +15,11 @@ import * as Sentry from '@sentry/react-native';
 import LottieView from 'lottie-react-native';
 import { Keyboard, StyleSheet } from 'react-native';
 import useAxios from '../../../core/HttpRequest';
-import { ButtonType, CardProviders, GlobalContextType } from '../../../constants/enum';
+import {
+  ButtonType,
+  CardProviders,
+  GlobalContextType,
+} from '../../../constants/enum';
 import clsx from 'clsx';
 import { isAndroid } from '../../../misc/checkers';
 import Button from '../../../components/v2/button';
@@ -18,7 +29,16 @@ import { GlobalContext } from '../../../core/globalContext';
 import { screenTitle } from '../../../constants';
 import { useKeyboard } from '../../../hooks/useKeyboard';
 
-export default function ActivateCard (props: {navigation: any, route: {params: {onSuccess: (data: any, provider: CardProviders) => {}, currentCardProvider: CardProviders, card: {cardId: string}}}}) {
+export default function ActivateCard(props: {
+  navigation: any;
+  route: {
+    params: {
+      onSuccess: (data: any, provider: CardProviders) => void;
+      currentCardProvider: CardProviders;
+      card: { cardId: string };
+    };
+  };
+}) {
   const { t } = useTranslation();
   const { showModal, hideModal } = useGlobalModalContext();
   const [sendingOTP, setSendingOTP] = useState<boolean>(false);
@@ -54,7 +74,13 @@ export default function ActivateCard (props: {navigation: any, route: {params: {
       const response = await postWithAuth(triggerOTPUrl, {});
       return !response.isError;
     } catch (e: any) {
-      showModal('state', { type: 'error', title: t('OTP_TRIGGER_FAILED'), description: e.mesaage, onSuccess: hideModal, onFailure: hideModal });
+      showModal('state', {
+        type: 'error',
+        title: t('OTP_TRIGGER_FAILED'),
+        description: e.mesaage,
+        onSuccess: hideModal,
+        onFailure: hideModal,
+      });
       Sentry.captureException(e);
       return false;
     }
@@ -66,7 +92,12 @@ export default function ActivateCard (props: {navigation: any, route: {params: {
     if (response) {
       setSendingOTP(false);
       let resendTime = resendOtpTime;
-      setTimer(setInterval(() => { resendTime--; setResendInterval(resendTime); }, 1000));
+      setTimer(
+        setInterval(() => {
+          resendTime--;
+          setResendInterval(resendTime);
+        }, 1000),
+      );
     }
   };
 
@@ -75,7 +106,13 @@ export default function ActivateCard (props: {navigation: any, route: {params: {
     setLoading(false);
     if (type === 'success') {
       setTimeout(() => {
-        navigation.navigate(screenTitle.CARD_SET_PIN_SCREEN, { onSuccess: (data: any, cardProvider: CardProviders) => { navigation.navigate(screenTitle.DEBIT_CARD_SCREEN); }, currentCardProvider, card });
+        navigation.navigate(screenTitle.CARD_SET_PIN_SCREEN, {
+          onSuccess: (data: any, cardProvider: CardProviders) => {
+            navigation.navigate(screenTitle.DEBIT_CARD_SCREEN);
+          },
+          currentCardProvider,
+          card,
+        });
       }, MODAL_HIDE_TIMEOUT);
     }
   };
@@ -83,8 +120,8 @@ export default function ActivateCard (props: {navigation: any, route: {params: {
   const activateCard = async () => {
     const verificationUrl = `/v1/cards/${currentCardProvider}/card/${card?.cardId}/activate-card`;
     const payload = {
-      otp: +(otp),
-      last4
+      otp: +otp,
+      last4,
     };
     setLoading(true);
     Keyboard.dismiss();
@@ -92,51 +129,73 @@ export default function ActivateCard (props: {navigation: any, route: {params: {
       const response = await patchWithAuth(verificationUrl, payload);
       if (!response.isError) {
         const data = await getWalletProfile(globalContext.globalState.token);
-        globalContext.globalDispatch({ type: GlobalContextType.CARD_PROFILE, cardProfile: data });
-        showModal('state', { type: 'success', title: t('ACTIVATION_SUCCESSFUL'), description: t('CARD_ACTIVATION_SUCCESSFUL'), onSuccess: () => onModalHide('success'), onFailure: () => onModalHide() });
+        globalContext.globalDispatch({
+          type: GlobalContextType.CARD_PROFILE,
+          cardProfile: data,
+        });
+        showModal('state', {
+          type: 'success',
+          title: t('ACTIVATION_SUCCESSFUL'),
+          description: t('CARD_ACTIVATION_SUCCESSFUL'),
+          onSuccess: () => onModalHide('success'),
+          onFailure: () => onModalHide(),
+        });
       } else {
-        showModal('state', { type: 'error', title: t('VERIFICATION_FAILED'), description: response.error.message ?? t('INVALID_DETAILS'), onSuccess: () => onModalHide(), onFailure: () => onModalHide() });
+        showModal('state', {
+          type: 'error',
+          title: t('VERIFICATION_FAILED'),
+          description: response.error.message ?? t('INVALID_DETAILS'),
+          onSuccess: () => onModalHide(),
+          onFailure: () => onModalHide(),
+        });
       }
     } catch (e: any) {
-      showModal('state', { type: 'error', title: t('VERIFICATION_FAILED'), description: t('INVALID_DETAILS'), onSuccess: () => onModalHide(), onFailure: () => onModalHide() });
+      showModal('state', {
+        type: 'error',
+        title: t('VERIFICATION_FAILED'),
+        description: t('INVALID_DETAILS'),
+        onSuccess: () => onModalHide(),
+        onFailure: () => onModalHide(),
+      });
       Sentry.captureException(e);
     }
   };
 
-  const ActivateCardHeader = () => {
-    return (
-      <CyDView className='px-[20px]'>
-        <CyDText className={'text-[25px] font-extrabold'}>{t<string>('CARD_ACTIVATION_HEADER')}</CyDText>
-        <CyDText className={'text-[15px] font-bold'}>{t<string>('CARD_ACTIVATION_DESCRIPTION')}</CyDText>
-      </CyDView>
-    );
-  };
-
-  const styles = StyleSheet.create({
-    lottie: {
-      height: 25
-    },
-    contentContainerStyle: {
-      display: 'flex',
-      flexDirection: 'column',
-      justifyContent: 'space-between',
-      ...(!keyboardHeight && { flex: 1 })
-    }
-  });
-
   return (
-    <CyDSafeAreaView style={{ height: keyboardHeight || '100%' }} >
-        <CyDScrollView className=' bg-white pb-[12px]' contentContainerStyle={styles.contentContainerStyle}>
-          <CyDView>
-            <ActivateCardHeader />
-            <CyDView className={' px-[24px] pt-[10px] mt-[14px]'}>
-            <CyDText className={'text-[18px] font-extrabold'}>{t<string>('Last 4 digits of card number')}</CyDText>
+    <CyDSafeAreaView style={{ height: keyboardHeight || '100%' }}>
+      <CyDScrollView
+        className=' bg-white pb-[12px]'
+        contentContainerStyle={{
+          ...styles.contentContainerStyle,
+          ...(!keyboardHeight && { flex: 1 }),
+        }}
+      >
+        <CyDView>
+          <CyDView className='px-[20px]'>
+            <CyDText className={'text-[25px] font-extrabold'}>
+              {t<string>('CARD_ACTIVATION_HEADER')}
+            </CyDText>
+            <CyDText className={'text-[15px] font-bold'}>
+              {t<string>('CARD_ACTIVATION_DESCRIPTION')}
+            </CyDText>
+          </CyDView>
+          <CyDView className={' px-[24px] pt-[10px] mt-[14px]'}>
+            <CyDText className={'text-[18px] font-extrabold'}>
+              {t<string>('Last 4 digits of card number')}
+            </CyDText>
             <CyDView>
               <CyDView className={'mt-[5px]'}>
                 {/* <OtpInput pinCount={4} getOtp={(otp) => { void verifyOTP(Number(otp)); }}></OtpInput> */}
                 <CyDTextInput
-                  className={clsx('h-[55px] text-center w-[100%] tracking-[2px] rounded-[5px] border-[1px] border-inputBorderColor', { 'pl-[1px] pt-[2px]': isAndroid(), 'tracking-[15px]': last4 !== '', 'border-redCyD': last4 !== '' && last4.length !== 4 })}
-                  keyboardType="numeric"
+                  className={clsx(
+                    'h-[55px] text-center w-[100%] tracking-[2px] rounded-[5px] border-[1px] border-inputBorderColor',
+                    {
+                      'pl-[1px] pt-[2px]': isAndroid(),
+                      'tracking-[15px]': last4 !== '',
+                      'border-redCyD': last4 !== '' && last4.length !== 4,
+                    },
+                  )}
+                  keyboardType='numeric'
                   placeholder='Enter last4 digits of card number'
                   placeholderTextColor={'#C5C5C5'}
                   onChangeText={(num: string) => setLast4(num)}
@@ -147,29 +206,74 @@ export default function ActivateCard (props: {navigation: any, route: {params: {
             </CyDView>
           </CyDView>
           <CyDView className={'px-[20px] mt-[30px]'}>
-            <CyDText className={'text-[18px] font-extrabold'}>{t<string>('OTP')}</CyDText>
+            <CyDText className={'text-[18px] font-extrabold'}>
+              {t<string>('OTP')}
+            </CyDText>
             <CyDView>
               <CyDView className={'mt-[5px]'}>
-                <OtpInput pinCount={4} getOtp={(otp) => { setOtp(otp); }} placeholder={t('ENTER_OTP')}></OtpInput>
-                <CyDTouchView className={'flex flex-row self-start items-center mt-[25px]'} disabled={sendingOTP || resendInterval !== 0} onPress={() => { void resendOTP(); }}>
-                  <CyDText className={'font-bold underline decoration-solid underline-offset-4'}>{t<string>('RESEND_CODE_INIT_CAPS')}</CyDText>
-                  {sendingOTP && <LottieView source={AppImages.LOADER_TRANSPARENT}
-                    autoPlay
-                    loop
-                    style={styles.lottie}
-                  />}
-                  {resendInterval !== 0 && <CyDText>{String(` in ${resendInterval} sec`)}</CyDText>}
+                <OtpInput
+                  pinCount={4}
+                  getOtp={(otp) => {
+                    setOtp(otp);
+                  }}
+                  placeholder={t('ENTER_OTP')}
+                />
+                <CyDTouchView
+                  className={'flex flex-row self-start items-center mt-[25px]'}
+                  disabled={sendingOTP || resendInterval !== 0}
+                  onPress={() => {
+                    void resendOTP();
+                  }}
+                >
+                  <CyDText
+                    className={
+                      'font-bold underline decoration-solid underline-offset-4'
+                    }
+                  >
+                    {t<string>('RESEND_CODE_INIT_CAPS')}
+                  </CyDText>
+                  {sendingOTP && (
+                    <LottieView
+                      source={AppImages.LOADER_TRANSPARENT}
+                      autoPlay
+                      loop
+                      style={styles.lottie}
+                    />
+                  )}
+                  {resendInterval !== 0 && (
+                    <CyDText>{String(` in ${resendInterval} sec`)}</CyDText>
+                  )}
                 </CyDTouchView>
-                </CyDView>
               </CyDView>
             </CyDView>
           </CyDView>
-          <CyDView className="w-full mb-[4px] mt-[12px] items-center">
-          <Button title={t('ACTIVATE')} disabled={!otp || !last4 || (otp.length !== 4 || last4.length !== 4)} onPress={() => {
-            void (async () => { await activateCard(); })();
-          }} type={ButtonType.PRIMARY} loading={loading} style=' h-[60px] w-[90%]'/>
-          </CyDView>
-        </CyDScrollView>
-      </CyDSafeAreaView>
+        </CyDView>
+        <CyDView className='w-full mb-[4px] mt-[12px] items-center'>
+          <Button
+            title={t('ACTIVATE')}
+            disabled={!otp || !last4 || otp.length !== 4 || last4.length !== 4}
+            onPress={() => {
+              void (async () => {
+                await activateCard();
+              })();
+            }}
+            type={ButtonType.PRIMARY}
+            loading={loading}
+            style=' h-[60px] w-[90%]'
+          />
+        </CyDView>
+      </CyDScrollView>
+    </CyDSafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  lottie: {
+    height: 25,
+  },
+  contentContainerStyle: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+  },
+});
--- src/containers/DebitCard/bridgeCard/cardRevealAuth.tsx
+++ src/containers/DebitCard/bridgeCard/cardRevealAuth.tsx
@@ -1,5 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import { CyDSafeAreaView, CyDText, CyDTouchView, CyDView } from '../../../styles/tailwindStyles';
+import {
+  CyDSafeAreaView,
+  CyDText,
+  CyDTouchView,
+  CyDView,
+} from '../../../styles/tailwindStyles';
 import { useTranslation } from 'react-i18next';
 import OtpInput from '../../../components/v2/OTPInput';
 import AppImages from '../../../../assets/images/appImages';
@@ -11,7 +16,18 @@ import { StyleSheet } from 'react-native';
 import useAxios from '../../../core/HttpRequest';
 import { CardProviders } from '../../../constants/enum';
 
-export default function CardRevealAuthScreen (props: {navigation: any, route: {params: {onSuccess: (data: any, provider: CardProviders) => {}, currentCardProvider: CardProviders, card: {cardId: string}, triggerOTPParam?: string, verifyOTPPayload?: any}}}) {
+export default function CardRevealAuthScreen(props: {
+  navigation: any;
+  route: {
+    params: {
+      onSuccess: (data: any, provider: CardProviders) => {};
+      currentCardProvider: CardProviders;
+      card: { cardId: string };
+      triggerOTPParam?: string;
+      verifyOTPPayload?: any;
+    };
+  };
+}) {
   const { t } = useTranslation();
   const { showModal, hideModal } = useGlobalModalContext();
   const [sendingOTP, setSendingOTP] = useState<boolean>(false);
@@ -36,12 +52,22 @@ export default function CardRevealAuthScreen (props: {navigation: any, route: {p
   }, [resendInterval]);
 
   const triggerOTP = async () => {
-    const triggerOTPUrl = `/v1/cards/${currentCardProvider}/card/${card.cardId}/trigger/${triggerOTPParam === 'verify/show-token' ? 'show-token' : triggerOTPParam}`;
+    const triggerOTPUrl = `/v1/cards/${currentCardProvider}/card/${
+      card.cardId
+    }/trigger/${
+      triggerOTPParam === 'verify/show-token' ? 'show-token' : triggerOTPParam
+    }`;
     try {
       const response = await postWithAuth(triggerOTPUrl, {});
       return !response.isError;
     } catch (e: any) {
-      showModal('state', { type: 'error', title: t('OTP_TRIGGER_FAILED'), description: e.mesaage, onSuccess: hideModal, onFailure: hideModal });
+      showModal('state', {
+        type: 'error',
+        title: t('OTP_TRIGGER_FAILED'),
+        description: e.mesaage,
+        onSuccess: hideModal,
+        onFailure: hideModal,
+      });
       Sentry.captureException(e);
       return false;
     }
@@ -53,7 +79,12 @@ export default function CardRevealAuthScreen (props: {navigation: any, route: {p
     if (response) {
       setSendingOTP(false);
       let resendTime = resendOtpTime;
-      setTimer(setInterval(() => { resendTime--; setResendInterval(resendTime); }, 1000));
+      setTimer(
+        setInterval(() => {
+          resendTime--;
+          setResendInterval(resendTime);
+        }, 1000),
+      );
     }
   };
 
@@ -66,8 +97,8 @@ export default function CardRevealAuthScreen (props: {navigation: any, route: {p
     const OTPVerificationUrl = `/v1/cards/${currentCardProvider}/card/${card?.cardId}/${triggerOTPParam}`;
     setVerifyingOTP(true);
     const payload = {
-      otp: +(num),
-      ...(verifyOTPPayload || {})
+      otp: +num,
+      ...(verifyOTPPayload || {}),
     };
     try {
       const response = await postWithAuth(OTPVerificationUrl, payload);
@@ -76,10 +107,22 @@ export default function CardRevealAuthScreen (props: {navigation: any, route: {p
         onSuccess(response.data, currentCardProvider);
         navigation.goBack();
       } else {
-        showModal('state', { type: 'error', title: t('VERIFICATION_FAILED'), description: t('INVALID_OTP'), onSuccess: () => onModalHide(), onFailure: () => onModalHide() });
+        showModal('state', {
+          type: 'error',
+          title: t('VERIFICATION_FAILED'),
+          description: t('INVALID_OTP'),
+          onSuccess: () => onModalHide(),
+          onFailure: () => onModalHide(),
+        });
       }
     } catch (e: any) {
-      showModal('state', { type: 'error', title: t('VERIFICATION_FAILED'), description: t('INVALID_OTP'), onSuccess: () => onModalHide(), onFailure: () => onModalHide() });
+      showModal('state', {
+        type: 'error',
+        title: t('VERIFICATION_FAILED'),
+        description: t('INVALID_OTP'),
+        onSuccess: () => onModalHide(),
+        onFailure: () => onModalHide(),
+      });
       Sentry.captureException(e);
     }
   };
@@ -87,8 +130,12 @@ export default function CardRevealAuthScreen (props: {navigation: any, route: {p
   const OTPHeader = () => {
     return (
       <CyDView>
-        <CyDText className={'text-[25px] font-extrabold'}>{t<string>('ENTER_AUTHENTICATION_CODE')}</CyDText>
-        <CyDText className={'text-[15px] font-bold'}>{t<string>('CARD_SENT_OTP')}</CyDText>
+        <CyDText className={'text-[25px] font-extrabold'}>
+          {t<string>('ENTER_AUTHENTICATION_CODE')}
+        </CyDText>
+        <CyDText className={'text-[15px] font-bold'}>
+          {t<string>('CARD_SENT_OTP')}
+        </CyDText>
       </CyDView>
     );
   };
@@ -96,20 +143,45 @@ export default function CardRevealAuthScreen (props: {navigation: any, route: {p
   return (
     <CyDSafeAreaView>
       <CyDView className={'h-full bg-white px-[20px] pt-[10px]'}>
-        <OTPHeader/>
+        <OTPHeader />
         <CyDView>
-          {!verifyingOTP && <CyDView className={'mt-[15%]'}>
-            <OtpInput pinCount={4} getOtp={(otp) => { void verifyOTP(Number(otp)); }}></OtpInput>
-            <CyDTouchView className={'flex flex-row items-center mt-[15%]'} disabled={sendingOTP || resendInterval !== 0} onPress={() => { void resendOTP(); }}>
-              <CyDText className={'font-bold underline decoration-solid underline-offset-4'}>{t<string>('RESEND_CODE_INIT_CAPS')}</CyDText>
-              {sendingOTP && <LottieView source={AppImages.LOADER_TRANSPARENT}
-                autoPlay
-                loop
-                style={styles.lottie}
-              />}
-              {resendInterval !== 0 && <CyDText>{String(` in ${resendInterval} sec`)}</CyDText>}
-            </CyDTouchView>
-          </CyDView>}
+          {!verifyingOTP && (
+            <CyDView className={'mt-[15%]'}>
+              <OtpInput
+                pinCount={4}
+                getOtp={(otp) => {
+                  void verifyOTP(Number(otp));
+                }}
+                placeholder={t('ENTER_OTP')}
+              />
+              <CyDTouchView
+                className={'flex flex-row items-center mt-[15%]'}
+                disabled={sendingOTP || resendInterval !== 0}
+                onPress={() => {
+                  void resendOTP();
+                }}
+              >
+                <CyDText
+                  className={
+                    'font-bold underline decoration-solid underline-offset-4'
+                  }
+                >
+                  {t<string>('RESEND_CODE_INIT_CAPS')}
+                </CyDText>
+                {sendingOTP && (
+                  <LottieView
+                    source={AppImages.LOADER_TRANSPARENT}
+                    autoPlay
+                    loop
+                    style={styles.lottie}
+                  />
+                )}
+                {resendInterval !== 0 && (
+                  <CyDText>{String(` in ${resendInterval} sec`)}</CyDText>
+                )}
+              </CyDTouchView>
+            </CyDView>
+          )}
           {verifyingOTP && <Loading />}
         </CyDView>
       </CyDView>
@@ -119,6 +191,6 @@ export default function CardRevealAuthScreen (props: {navigation: any, route: {p
 
 const styles = StyleSheet.create({
   lottie: {
-    height: 25
-  }
+    height: 25,
+  },
 });
