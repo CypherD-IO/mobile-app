9636e81bac467e300510a641a029d4457aa35e7b
karthickcypher <karthick@cypherd.io>
2023-11-23 11:27:14 +0530

    feat: :sparkles: privacy toggle disabled outside privacy mode
    disabled the onPress of the TokenAmount and TokenValue fields outside of Privacy mode being enabled

--- src/components/v2/tokenValue.tsx
+++ src/components/v2/tokenValue.tsx
@@ -3,12 +3,13 @@ import React, { useContext, useEffect, useState } from 'react';
 import { HdWalletContext, limitDecimalPlaces } from '../../core/util';
 import { CyDText } from '../../styles/tailwindStyles';
 
-export default function CyDTokenValue (props?: any) {
+export default function CyDTokenValue(props?: any) {
   const hdWallet = useContext<any>(HdWalletContext);
   const { hideBalance } = hdWallet.state;
   const { children, revealTemporarily = false } = props;
   const hideWithCharacters = '******';
-  const [overrideHideBalance, setOverrideHideBalance] = useState(revealTemporarily);
+  const [overrideHideBalance, setOverrideHideBalance] =
+    useState(revealTemporarily);
 
   useEffect(() => {
     if (hideBalance) {
@@ -18,12 +19,19 @@ export default function CyDTokenValue (props?: any) {
 
   const formatTokenValue = new Intl.NumberFormat('en-US', {
     style: 'currency',
-    currency: 'USD'
+    currency: 'USD',
   });
 
   return (
-    <CyDText onPress={() => { hideBalance && setOverrideHideBalance(!overrideHideBalance); }} {...props} >
-      {(hideBalance && !overrideHideBalance) ? hideWithCharacters : formatTokenValue.format(children)}
+    <CyDText
+      onPress={() => {
+        hideBalance && setOverrideHideBalance(!overrideHideBalance);
+      }}
+      disabled={!hideBalance}
+      {...props}>
+      {hideBalance && !overrideHideBalance
+        ? hideWithCharacters
+        : formatTokenValue.format(children)}
     </CyDText>
   );
 }
