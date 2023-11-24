9177d66eba1e14babd6a24282fce4d62f0074e87
Karthickpranav S N <132334547+karthickcypher@users.noreply.github.com>
2023-11-24 14:51:45 +0530

    feat: :sparkles: non-decoded txns warning, adjust pairing modal styles (#177)
    Added a warning message for the the transactions that are not decoded. Also changed the styles of the Pairing modal to make it consistent with the other modals.

--- src/components/v2/walletConnectV2Views/SigningModals/SignMessageModal.tsx
+++ src/components/v2/walletConnectV2Views/SigningModals/SignMessageModal.tsx
@@ -1,23 +1,39 @@
 import React from 'react';
 import { CyDView } from '../../../../styles/tailwindStyles';
-import { Divider, RenderDAPPInfo, RenderMessage, RenderMethod, RenderNetwork } from './SigningModalComponents';
+import {
+  Divider,
+  RenderDAPPInfo,
+  RenderMessage,
+  RenderMethod,
+  RenderNetwork,
+} from './SigningModalComponents';
 import { IDAppInfo } from '../../../../models/signingModalData.interface';
 import { Chain } from '../../../../constants/server';
 
-export const RenderSignMessageModal = ({ dAppInfo, chain, method, messageParams }: {dAppInfo: IDAppInfo | undefined, chain: Chain | undefined, method: string, messageParams: any}) => {
+export const RenderSignMessageModal = ({
+  dAppInfo,
+  chain,
+  method,
+  messageParams,
+}: {
+  dAppInfo: IDAppInfo | undefined;
+  chain: Chain | undefined;
+  method: string;
+  messageParams: any;
+}) => {
   return (
-      <CyDView>
-        {dAppInfo
-          ? <>
-        <RenderDAPPInfo dAppInfo={dAppInfo} />
-        <Divider />
-         </>
-          : null}
-        <RenderNetwork chain={chain} />
-        <Divider />
-        <RenderMethod method={method} />
-        <Divider />
-        <RenderMessage messageParams={messageParams} method={method} />
-      </CyDView>
+    <CyDView>
+      {dAppInfo ? (
+        <>
+          <RenderDAPPInfo dAppInfo={dAppInfo} />
+          <Divider />
+        </>
+      ) : null}
+      <RenderNetwork chain={chain} />
+      <Divider />
+      <RenderMethod method={method} />
+      <Divider />
+      <RenderMessage messageParams={messageParams} method={method} />
+    </CyDView>
   );
 };
