fe7d249461c290b93f3f2f8bb04aebe802c54a73
shan-c <shanthan@cypherd.io>
2023-12-20 18:46:32 +0530

    Removed eth address listener in QR code scanner screen

--- src/containers/Qrcode/QRScanner.tsx
+++ src/containers/Qrcode/QRScanner.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useState } from 'react';
 import QRCodeScanner from 'react-native-qrcode-scanner';
 import { BackHandler, Dimensions, ImageBackground } from 'react-native';
 import { useTranslation } from 'react-i18next';
@@ -12,11 +12,6 @@ import { QRScannerScreens } from '../../constants/server';
 import { BarCodeReadEvent } from 'react-native-camera';
 import AppImages from '../../../assets/images/appImages';
 import * as C from '../../constants/index';
-import {
-  HdWalletContext,
-  _NO_CYPHERD_CREDENTIAL_AVAILABLE_,
-} from '../../core/util';
-import Loading from '../../components/v2/loading';
 
 const SCREEN_HEIGHT = Dimensions.get('window').height;
 
@@ -31,8 +26,6 @@ export default function QRScanner(props: {
 }) {
   const { t } = useTranslation();
   const [fromPage] = useState<string>(props.route.params.fromPage);
-  const hdWalletContext = useContext<any>(HdWalletContext);
-  const ethereum = hdWalletContext.state.wallet.ethereum;
 
   const renderText = () => {
     switch (fromPage) {
@@ -63,57 +56,53 @@ export default function QRScanner(props: {
 
   return (
     <CyDScrollView>
-      {ethereum.address === _NO_CYPHERD_CREDENTIAL_AVAILABLE_ ? (
-        <Loading />
-      ) : (
-        <QRCodeScanner
-          showMarker
-          fadeIn={false}
-          onRead={e => {
-            props?.navigation?.goBack();
-            props?.route?.params?.onSuccess(e);
-          }}
-          cameraStyle={{ height: SCREEN_HEIGHT }}
-          customMarker={
-            <ImageBackground
-              source={AppImages.SCANNER_BG}
-              resizeMode='cover'
-              style={{ height: '100%', width: '100%', marginTop: '-30%' }}
-              imageStyle={{ height: '100%', width: '100%' }}>
+      <QRCodeScanner
+        showMarker
+        fadeIn={false}
+        onRead={e => {
+          props?.navigation?.goBack();
+          props?.route?.params?.onSuccess(e);
+        }}
+        cameraStyle={{ height: SCREEN_HEIGHT }}
+        customMarker={
+          <ImageBackground
+            source={AppImages.SCANNER_BG}
+            resizeMode='cover'
+            style={{ height: '100%', width: '100%', marginTop: '-30%' }}
+            imageStyle={{ height: '100%', width: '100%' }}>
+            <CyDView
+              className={
+                'flex flex-column justify-end items-center h-screen w-screen'
+              }>
               <CyDView
                 className={
-                  'flex flex-column justify-end items-center h-screen w-screen'
+                  'flex items-center justify-center h-1/2 w-10/12 pt-[10px]'
                 }>
-                <CyDView
+                <CyDText
                   className={
-                    'flex items-center justify-center h-1/2 w-10/12 pt-[10px]'
+                    'font-nunito text-center text-[20px] color-white mt-[24%]'
                   }>
-                  <CyDText
+                  {renderText()}
+                </CyDText>
+                {fromPage === QRScannerScreens.WALLET_CONNECT && (
+                  <CyDTouchView
                     className={
-                      'font-nunito text-center text-[20px] color-white mt-[24%]'
+                      'flex items-center justify-center mt-[40px] h-[60px] w-2/3 border-[1px] border-[#8E8E8E] rounded-[12px]'
+                    }
+                    onPress={() =>
+                      props.navigation.navigate(C.screenTitle.WALLET_CONNECT)
                     }>
-                    {renderText()}
-                  </CyDText>
-                  {fromPage === QRScannerScreens.WALLET_CONNECT && (
-                    <CyDTouchView
-                      className={
-                        'flex items-center justify-center mt-[40px] h-[60px] w-2/3 border-[1px] border-[#8E8E8E] rounded-[12px]'
-                      }
-                      onPress={() =>
-                        props.navigation.navigate(C.screenTitle.WALLET_CONNECT)
-                      }>
-                      <CyDText
-                        className={'text-white text-[15px] font-extrabold'}>
-                        {t('Manage Connections')}
-                      </CyDText>
-                    </CyDTouchView>
-                  )}
-                </CyDView>
+                    <CyDText
+                      className={'text-white text-[15px] font-extrabold'}>
+                      {t('Manage Connections')}
+                    </CyDText>
+                  </CyDTouchView>
+                )}
               </CyDView>
-            </ImageBackground>
-          }
-        />
-      )}
+            </CyDView>
+          </ImageBackground>
+        }
+      />
     </CyDScrollView>
   );
 }
