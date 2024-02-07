013a37fb108680bd0f1fd250b22324fec1b091e8
shan-c <shanthan@cypherd.io>
2024-02-07 15:15:09 +0530

    Added wallet connect disconnection

--- src/components/walletConnectV2Provider/index.tsx
+++ src/components/walletConnectV2Provider/index.tsx
@@ -8,7 +8,6 @@ import React, {
 } from 'react';
 import {
   HdWalletContext,
-  PortfolioContext,
   _NO_CYPHERD_CREDENTIAL_AVAILABLE_,
 } from '../../core/util';
 import {
@@ -17,37 +16,9 @@ import {
 } from '../../core/walletConnectV2Utils';
 import useInitialIntentURL from '../../hooks/useInitialIntentURL';
 import useWalletConnectEventsManager from '../../hooks/useWalletConnectV2EventsManager';
-import { WalletConnectActions } from '../../reducers/wallet_connect_reducer';
 import * as Sentry from '@sentry/react-native';
 import { Config } from 'react-native-config';
-import {
-  mainnet,
-  polygon,
-  optimism,
-  arbitrum,
-  avalanche,
-  fantom,
-  bsc,
-  evmos,
-  zkSync,
-  base,
-  polygonZkEvm,
-  aurora,
-  moonbeam,
-  moonriver,
-} from 'viem/chains';
-import {
-  createWeb3Modal,
-  defaultWagmiConfig,
-} from '@web3modal/wagmi-react-native';
-import { WagmiConfig } from 'wagmi';
-import WalletConnectListener from '../walletConnectListener';
-import { WalletConnectConnector } from 'wagmi/connectors/walletConnect';
-import { configureChains, createConfig, type Chain } from 'wagmi';
-import { publicProvider } from 'wagmi/providers/public';
-import { CoinbaseWagmiConnector } from '@web3modal/coinbase-react-native';
-import { walletConnectProvider } from '../../core/walletConnectProvider';
-import WagmiConfigBuilder from '../wagmiConfigBuilder';
+import { WagmiConfigBuilder } from '../wagmiConfigBuilder';
 
 const walletConnectInitialValue = {
   initialized: false,
@@ -57,22 +28,16 @@ export const WalletConnectContext = createContext(walletConnectInitialValue);
 
 export const WalletConnectV2Provider: React.FC<any> = ({ children }) => {
   // Step 1 - Initialize wallets and wallet connect client
-  const portfolioState = useContext<any>(PortfolioContext);
   const hdWalletContext = useContext<any>(HdWalletContext);
   const ethereum = hdWalletContext.state.wallet.ethereum;
-  const { walletConnectDispatch } = useContext<any>(WalletConnectContext);
   const isInitializationInProgress = useRef<boolean>(false);
   const projectId = String(Config.WALLET_CONNECT_PROJECTID);
-  // const { isConnected, provider, address } = useWalletConnectModal()
 
   // Step 2 - Once initialized, set up wallet connect event manager
 
   const [isWeb3WalletInitialized, setIsWeb3WalletInitialized] =
     useState<boolean>(false);
   useWalletConnectEventsManager(isWeb3WalletInitialized);
-  // const prevRelayerURLValue = useRef<string>('')
-
-  // const { relayerRegionURL } = useSnapshot(SettingsStore.state)
 
   const onInitialize = useCallback(async () => {
     // const resp = await getWithAuth('/v1/authentication/creds/wc'); //TO DO Eliminate sign message race condition (axios intercept)
@@ -90,70 +55,6 @@ export const WalletConnectV2Provider: React.FC<any> = ({ children }) => {
     }
   }, []);
 
-  const metadata = {
-    name: 'Cypher Wallet',
-    description: 'Cypher Wallet',
-    url: 'https://cypherwallet.io',
-    icons: ['https://avatars.githubusercontent.com/u/37784886'],
-    redirect: {
-      native: 'cypherwallet://',
-      universal: 'YOUR_APP_UNIVERSAL_LINK.com',
-    },
-  };
-
-  const chains = [
-    mainnet,
-    polygon,
-    optimism,
-    arbitrum,
-    avalanche,
-    fantom,
-    bsc,
-    evmos,
-    zkSync,
-    base,
-    polygonZkEvm,
-    aurora,
-    moonbeam,
-    moonriver,
-  ];
-
-  // const walletConnectConnector = new WalletConnectConnector({
-  //   chains,
-  //   options: { projectId, showQrModal: false, metadata },
-  // });
-
-  // const { publicClient } = configureChains(chains, [
-  //   walletConnectProvider({ projectId }),
-  //   publicProvider(),
-  // ]);
-
-  // const coinbaseConnector = new CoinbaseWagmiConnector({
-  //   chains,
-  //   options: {
-  //     redirect: 'cypherwallet://',
-  //   },
-  // });
-
-  // const wagmiConfig = defaultWagmiConfig({
-  //   chains,
-  //   projectId,
-  //   metadata,
-  // });
-
-  // const wagmiConfig = createConfig({
-  //   autoConnect: true,
-  //   connectors: [walletConnectConnector, coinbaseConnector],
-  //   publicClient,
-  // });
-
-  // 3. Create modal
-  // createWeb3Modal({
-  //   projectId,
-  //   chains,
-  //   wagmiConfig,
-  // });
-
   useEffect(() => {
     if (
       !isWeb3WalletInitialized &&
@@ -164,10 +65,6 @@ export const WalletConnectV2Provider: React.FC<any> = ({ children }) => {
       isInitializationInProgress.current = true;
       void onInitialize();
     }
-    // if (prevRelayerURLValue.current !== relayerRegionURL) {
-    //   setInitialized(false);
-    //   onInitialize();
-    // }
   }, [isWeb3WalletInitialized, ethereum.address]);
 
   const { url: initialUrl } = useInitialIntentURL();
@@ -205,10 +102,6 @@ export const WalletConnectV2Provider: React.FC<any> = ({ children }) => {
     <WalletConnectContext.Provider
       value={{ initialized: isWeb3WalletInitialized }}>
       <WagmiConfigBuilder>{children}</WagmiConfigBuilder>
-
-      {/* <WagmiConfig config={wagmiConfig}>
-        <WalletConnectListener>{children}</WalletConnectListener>
-      </WagmiConfig> */}
     </WalletConnectContext.Provider>
   );
 };
