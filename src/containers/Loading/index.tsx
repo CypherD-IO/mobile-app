28765f3ba61d941e395ebda4d9e0fcf4a367c8c6
shan-c <shanthan@cypherd.io>
2024-03-11 22:38:23 +0530

    changed loader component

--- src/containers/Loading/index.tsx
+++ src/containers/Loading/index.tsx
@@ -11,8 +11,8 @@ import { CyDSafeAreaView, CyDText, CyDView } from '../../styles/tailwindStyles';
 export default function Loading({ loadingText }: { loadingText?: string }) {
   const { t } = useTranslation();
   return (
-    <CyDSafeAreaView>
-      <CyDView className='flex-1 h-full w-full justify-center'>
+    <CyDSafeAreaView className='h-full'>
+      <CyDView className='flex flex-1 flex-col h-full w-[70%] justify-center self-center'>
         <ActivityIndicator size='large' color={Colors.appColor} />
         <CyDText className='text-center font-nunito text-[14px] mt-[10px]'>
           {loadingText ?? t('LOADING_TEXT')}
