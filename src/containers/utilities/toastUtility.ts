fb721e9abccba7dd759dcec7cc94c760ad9120b8
shan-c <shanthan@cypherd.io>
2024-03-19 15:00:07 +0530

    Changed toast util style

--- src/containers/utilities/toastUtility.ts
+++ src/containers/utilities/toastUtility.ts
@@ -2,7 +2,7 @@ import Toast from 'react-native-toast-message';
 
 export const showToast = (message: string) => {
   Toast.show({
-    type: 'simpleToast',
-    props: { text: message, image: false }
+    type: 'success',
+    text1: message,
   });
 };
