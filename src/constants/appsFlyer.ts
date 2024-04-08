6156477546381432e527fc9736b2004cf24effb7
shan-c <114131317+shan-c@users.noreply.github.com>
2024-04-08 14:32:49 +0530

    removed appsflyer integration (#253)

--- src/constants/appsFlyer.ts
+++
@@ -1,7 +0,0 @@
-export const AppsFlyerConfiguration = {
-  appId: 'id1604120414',
-  devKey: 'wGbeNLrPrqVXjtgryufVv5',
-  brandedDomains: ['getapp.cypherwallet.io'],
-  oneLinkId: 'YxOA',
-  userInviteChannel: 'UserInvite',
-};
