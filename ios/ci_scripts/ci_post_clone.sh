bfc09b11e45eea54c8685014d9965e4422cb6c15
mohanramCypherD <mohanram@cypherd.io>
2024-08-21 12:24:02 +0530

    fix: logs added for ios

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -1,15 +1,30 @@
 #!/bin/sh
 chmod +x ci_post_clone.sh
+
+cd /ios
+echo pwd
+
+echo "exporting HOMEBREW_NO_INSTALL_CLEANUP=TRUE"
 export HOMEBREW_NO_INSTALL_CLEANUP=TRUE
+
+echo "installing cocoapods"
 brew install cocoapods
+
 # have to add node yourself
+echo "installing node@18"
 brew install node@18
+
 # link it to the path
+echo "linking node@18"
 brew link node@18
 
 # Install dependencies you manage with CocoaPods.
+echo "installing packages"
 npm install --legacy-peer-deps
+echo "pod installing"
 pod install
+
 # the sed command from RN cant find the file... so we have to run it ourselves
+echo "sed command"
 sed -i -e  $'s/ && (__IPHONE_OS_VERSION_MIN_REQUIRED < __IPHONE_10_0)//' /Volumes/workspace/repository/ios/Pods/RCT-Folly/folly/portability/Time.h
 
