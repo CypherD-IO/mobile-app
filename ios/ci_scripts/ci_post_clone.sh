25280246e72f64ce9a631d1770cf0fb2f6f9201e
mohanramCypherD <mohanram@cypherd.io>
2024-08-21 17:53:51 +0530

    fix: added logs to debug 401 issue

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -15,26 +15,33 @@ brew install node@18
 echo "linking node@18"
 brew link node@18
 
-echo `export PATH="/usr/local/opt/node@18/bin:$PATH"` >> ~/.zshrc
+echo 'export PATH="/usr/local/opt/node@18/bin:$PATH"' >> ~/.zshrc
 
-# setting the auth token
+# Verify NPM_TOKEN
+echo "NPM_TOKEN: ${NPM_TOKEN:0:10}..."
+
+# Configure npm to use the token
+echo "//npm.pkg.github.com/:_authToken=${NPM_TOKEN}" > ~/.npmrc
+echo "@cypherd-io:registry=https://npm.pkg.github.com" >> ~/.npmrc
+
+# Set npm config
 npm config set @cypherd-io:registry https://npm.pkg.github.com
 npm config set //npm.pkg.github.com/:_authToken ${NPM_TOKEN}
 
+# Debug npm configuration
+echo "Debugging npm configuration:"
+npm config list
 
-echo "//npm.pkg.github.com/:_authToken=${NPM_TOKEN}" > ~/.npmrc
-npm config set @cypherd-io:registry https://npm.pkg.github.com
+# Clear npm cache
+echo "Clearing npm cache"
+npm cache clean --force
 
-# Install dependencies you manage with CocoaPods.
-echo "clean installing packages"
-npm ci --legacy-peer-deps
+# Install dependencies
+echo "Installing packages"
+npm ci --legacy-peer-deps --verbose
 
 pod --version
 
 echo "pod installing"
 pod install
-
-# the sed command from RN cant find the file... so we have to run it ourselves
-# echo "sed command"
-# sed -i -e  $'s/ && (__IPHONE_OS_VERSION_MIN_REQUIRED < __IPHONE_10_0)//' /Volumes/workspace/repository/ios/Pods/RCT-Folly/folly/portability/Time.h
 
