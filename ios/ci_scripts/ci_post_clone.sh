c9875f370450d63e4183ab54457550c62336ac56
mohanramCypherD <mohanram@cypherd.io>
2024-08-21 18:30:27 +0530

    fix: removed @cypherd-io/osmojs

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -15,32 +15,13 @@ brew install node@18
 echo "linking node@18"
 brew link node@18
 
-echo 'export PATH="/usr/local/opt/node@18/bin:$PATH"' >> ~/.zshrc
-
-# Verify NPM_TOKEN
-echo "NPM_TOKEN: ${NPM_TOKEN:0:10}..."
-
-# Configure npm to use the token
-echo "//npm.pkg.github.com/:_authToken=${NPM_TOKEN}" > ~/.npmrc
-echo "@cypherd-io:registry=https://npm.pkg.github.com" >> ~/.npmrc
-
-# Set npm config
-npm config set @cypherd-io:registry https://npm.pkg.github.com
-npm config set //npm.pkg.github.com/:_authToken ${NPM_TOKEN}
-
-# Debug npm configuration
-echo "Debugging npm configuration:"
-npm config list
-
 # Clear npm cache
 echo "Clearing npm cache"
 npm cache clean --force
 
 # Install dependencies
 echo "Installing packages"
-npm ci --legacy-peer-deps --verbose
-
-pod --version
+npm ci --legacy-peer-deps
 
 echo "pod installing"
 pod install
