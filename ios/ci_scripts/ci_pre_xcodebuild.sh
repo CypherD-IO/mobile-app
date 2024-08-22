5759b149295bd09f3c68130953374db29d1acdfe
mohanramCypherD <mohanram@cypherd.io>
2024-08-22 11:21:10 +0530

    fix: build number tobe added from info plist

---
+++ ios/ci_scripts/ci_pre_xcodebuild.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# Path to the Info.plist file
+INFO_PLIST_PATH="${PROJECT_DIR}/ios/CypherD/Info.plist"
+
+# Extract the current version and build number
+VERSION=$(/usr/libexec/PlistBuddy -c "Print CFBundleShortVersionString" "$INFO_PLIST_PATH")
+BUILD_NUMBER=$(/usr/libexec/PlistBuddy -c "Print CFBundleVersion" "$INFO_PLIST_PATH")
+
+# Update the Info.plist with the new values
+/usr/libexec/PlistBuddy -c "Set :CFBundleShortVersionString $VERSION" "$INFO_PLIST_PATH"
+/usr/libexec/PlistBuddy -c "Set :CFBundleVersion $BUILD_NUMBER" "$INFO_PLIST_PATH"
+
+echo "Updated Info.plist with version $VERSION and build number $BUILD_NUMBER"
