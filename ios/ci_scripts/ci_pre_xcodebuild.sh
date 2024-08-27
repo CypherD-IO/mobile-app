8862af031f308dc0cdd50fa0704e44f30ef63ec8
mohanramCypherD <mohanram@cypherd.io>
2024-08-27 15:28:48 +0530

    adding the sentry.properties .env and GoogleService-Info.plist

--- ios/ci_scripts/ci_pre_xcodebuild.sh
+++ ios/ci_scripts/ci_pre_xcodebuild.sh
@@ -14,3 +14,44 @@ BUILD_NUMBER=$(/usr/libexec/PlistBuddy -c "Print CFBundleVersion" "$INFO_PLIST_P
 /usr/libexec/PlistBuddy -c "Set :CFBundleVersion $BUILD_NUMBER" "$INFO_PLIST_PATH"
 
 echo "Updated Info.plist with version $VERSION and build number $BUILD_NUMBER"
+
+# adding the sentry.properties .env and GoogleService-Info.plist
+echo "updating the sentry.properties, .env and GoogleService-Info.plist files"
+
+# Path to the .env file
+ENV_FILE="${SRCROOT}/.env"
+echo "SENTRY_DSN=${SENTRY_DSN}" > $ENV_FILE
+echo "ENVIROINMENT=${ENVIROINMENT}" >> $ENV_FILE
+echo "AF_DEVKEY=${AF_DEVKEY}" >> $ENV_FILE
+echo "AF_APPID=${AF_APPID}" >> $ENV_FILE
+echo "AF_BARNDED_DOMAINS=${AF_BARNDED_DOMAINS}" >> $ENV_FILE
+echo "AF_ONE_LINK_ID=${AF_ONE_LINK_ID}" >> $ENV_FILE
+echo "AF_USER_INVITE_CHANNEL=${AF_USER_INVITE_CHANNEL}" >> $ENV_FILE
+echo "INTERCOM_APP_KEY=${INTERCOM_APP_KEY}" >> $ENV_FILE
+echo "WALLET_CONNECT_PROJECTID=${WALLET_CONNECT_PROJECTID}" >> $ENV_FILE
+
+echo "Updated .env with environment variables"
+
+# Path to the sentry.properties file
+SENTRY_FILE="${SRCROOT}/ios/sentry.properties"
+echo "defaults.url=https://sentry.io/" > $SENTRY_FILE
+echo "defaults.org=${SENTRY_ORG}" >> $SENTRY_FILE
+echo "defaults.project=${SENTRY_PROJECT}" >> $SENTRY_FILE
+echo "auth.token=${SENTRY_TOKEN}" >> $SENTRY_FILE
+
+echo "Updated sentry.properties with environment variables"
+
+# Path to the GoogleService-Info.plist file
+PLIST_PATH="${SRCROOT}/ios/CypherD/GoogleService-Info.plist"
+
+# Update GoogleService-Info.plist with environment variables
+/usr/libexec/PlistBuddy -c "Set :CLIENT_ID ${GOOGLE_CLIENT_ID}" "$PLIST_PATH"
+/usr/libexec/PlistBuddy -c "Set :REVERSED_CLIENT_ID ${GOOGLE_REVERSED_CLIENT_ID}" "$PLIST_PATH"
+/usr/libexec/PlistBuddy -c "Set :API_KEY ${GOOGLE_API_KEY}" "$PLIST_PATH"
+/usr/libexec/PlistBuddy -c "Set :GCM_SENDER_ID ${GOOGLE_GCM_SENDER_ID}" "$PLIST_PATH"
+/usr/libexec/PlistBuddy -c "Set :BUNDLE_ID ${GOOGLE_BUNDLE_ID}" "$PLIST_PATH"
+/usr/libexec/PlistBuddy -c "Set :PROJECT_ID ${GOOGLE_PROJECT_ID}" "$PLIST_PATH"
+/usr/libexec/PlistBuddy -c "Set :STORAGE_BUCKET ${GOOGLE_STORAGE_BUCKET}" "$PLIST_PATH"
+/usr/libexec/PlistBuddy -c "Set :GOOGLE_APP_ID ${GOOGLE_APP_ID}" "$PLIST_PATH"
+
+echo "Updated GoogleService-Info.plist with environment variables"
