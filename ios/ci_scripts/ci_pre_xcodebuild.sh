c74dc4290ef971f1b1aa9afc56757381c9587e7c
mohanramCypherD <mohanram@cypherd.io>
2024-08-27 23:23:48 +0530

    no env file created in pre script

--- ios/ci_scripts/ci_pre_xcodebuild.sh
+++ ios/ci_scripts/ci_pre_xcodebuild.sh
@@ -27,25 +27,6 @@ BUILD_NUMBER=$(/usr/libexec/PlistBuddy -c "Print CFBundleVersion" "${INFO_PLIST_
 echo "Updated Info.plist with version ${VERSION} and build number ${BUILD_NUMBER}"
 echo "File modified: ${INFO_PLIST_PATH}"
 
-# Path to the .env file in the root folder
-ENV_FILE="${SRCROOT}/.env"
-
-# Create .env file with environment variables
-cat << EOF > "${ENV_FILE}"
-SENTRY_DSN=${SENTRY_DSN}
-ENVIROINMENT=${ENVIROINMENT}
-AF_DEVKEY=${AF_DEVKEY}
-AF_APPID=${AF_APPID}
-AF_BARNDED_DOMAINS=${AF_BARNDED_DOMAINS}
-AF_ONE_LINK_ID=${AF_ONE_LINK_ID}
-AF_USER_INVITE_CHANNEL=${AF_USER_INVITE_CHANNEL}
-INTERCOM_APP_KEY=${INTERCOM_APP_KEY}
-WALLET_CONNECT_PROJECTID=${WALLET_CONNECT_PROJECTID}
-EOF
-
-echo "Created/Updated .env file in the root folder"
-echo "File modified: ${ENV_FILE}"
-
 # Path to the sentry.properties file
 SENTRY_FILE="${IOS_DIR}/sentry.properties"
 
@@ -63,22 +44,7 @@ echo "File modified: ${SENTRY_FILE}"
 # Path to the GoogleService-Info.plist file
 PLIST_PATH="${CYPHERD_DIR}/GoogleService-Info.plist"
 
-# Create or update GoogleService-Info.plist
-/usr/libexec/PlistBuddy -c "Clear dict" "${PLIST_PATH}" 2>/dev/null || true
-/usr/libexec/PlistBuddy -c "Add :CLIENT_ID string ${GOOGLE_CLIENT_ID}" "${PLIST_PATH}"
-/usr/libexec/PlistBuddy -c "Add :REVERSED_CLIENT_ID string ${GOOGLE_REVERSED_CLIENT_ID}" "${PLIST_PATH}"
-/usr/libexec/PlistBuddy -c "Add :API_KEY string ${GOOGLE_API_KEY}" "${PLIST_PATH}"
-/usr/libexec/PlistBuddy -c "Add :GCM_SENDER_ID string ${GOOGLE_GCM_SENDER_ID}" "${PLIST_PATH}"
-/usr/libexec/PlistBuddy -c "Add :PLIST_VERSION string 1" "${PLIST_PATH}"
-/usr/libexec/PlistBuddy -c "Add :BUNDLE_ID string ${GOOGLE_BUNDLE_ID}" "${PLIST_PATH}"
-/usr/libexec/PlistBuddy -c "Add :PROJECT_ID string ${GOOGLE_PROJECT_ID}" "${PLIST_PATH}"
-/usr/libexec/PlistBuddy -c "Add :STORAGE_BUCKET string ${GOOGLE_STORAGE_BUCKET}" "${PLIST_PATH}"
-/usr/libexec/PlistBuddy -c "Add :IS_ADS_ENABLED bool false" "${PLIST_PATH}"
-/usr/libexec/PlistBuddy -c "Add :IS_ANALYTICS_ENABLED bool false" "${PLIST_PATH}"
-/usr/libexec/PlistBuddy -c "Add :IS_APPINVITE_ENABLED bool true" "${PLIST_PATH}"
-/usr/libexec/PlistBuddy -c "Add :IS_GCM_ENABLED bool true" "${PLIST_PATH}"
-/usr/libexec/PlistBuddy -c "Add :IS_SIGNIN_ENABLED bool true" "${PLIST_PATH}"
-/usr/libexec/PlistBuddy -c "Add :GOOGLE_APP_ID string ${GOOGLE_APP_ID}" "${PLIST_PATH}"
+echo "${GOOGLE_SERVICE_INFO_PLIST}" > "${PLIST_PATH}"
 
 echo "Created/Updated GoogleService-Info.plist"
 echo "File modified: ${PLIST_PATH}"
