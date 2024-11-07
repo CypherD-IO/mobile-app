e189e30f5afc08d15101d2735cdc39d470e064da
mohanramCypherD <mohanram@cypherd.io>
2024-11-07 06:45:06 +0530

    added pod deintegate to post clone script

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -12,6 +12,7 @@ npm cache clean --force
 
 # Install dependencies you manage with CocoaPods.
 npm install --legacy-peer-deps
+pod deintegrate
 pod install --repo-update
 # the sed command from RN cant find the file... so we have to run it ourselves
 # sed -i -e  $'s/ && (__IPHONE_OS_VERSION_MIN_REQUIRED < __IPHONE_10_0)//' /Volumes/workspace/repository/ios/Pods/RCT-Folly/folly/portability/Time.h
