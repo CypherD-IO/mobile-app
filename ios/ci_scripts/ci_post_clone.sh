e06d80c746b81696ab0616ad00ab36d6a6fba763
mohanramCypherD <mohanram@cypherd.io>
2024-11-07 05:52:34 +0530

    updates xcode and macos versions

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -1,28 +1,14 @@
 #!/bin/sh
-chmod +x ci_post_clone.sh
 
-echo "exporting HOMEBREW_NO_INSTALL_CLEANUP=TRUE"
 export HOMEBREW_NO_INSTALL_CLEANUP=TRUE
-
-echo "installing cocoapods"
 brew install cocoapods
-
 # have to add node yourself
-echo "installing node@18"
-brew install node@18
+brew install node@16
+# link it to the path
+brew link node@16
 
-# linking it to the path
-echo "linking node@18"
-brew link node@18
-
-# Clear npm cache
-echo "Clearing npm cache"
-npm cache clean --force
-
-# Install dependencies
-echo "Installing packages"
-npm ci --legacy-peer-deps
-
-# Pod install
-echo "pod installing"
+# Install dependencies you manage with CocoaPods.
+npm install --legacy-peer-deps
 pod install
+# the sed command from RN cant find the file... so we have to run it ourselves
+sed -i -e  $'s/ && (__IPHONE_OS_VERSION_MIN_REQUIRED < __IPHONE_10_0)//' /Volumes/workspace/repository/ios/Pods/RCT-Folly/folly/portability/Time.h
