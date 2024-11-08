b875e3d2019b69870e9ce7f36bea61bc9594fbe9
mohanramCypherD <mohanram@cypherd.io>
2024-11-08 19:59:59 +0530

    node version 18 used

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -1,14 +1,11 @@
 #!/bin/sh
 
-echo $GOOGLE_SERVICE_INFO_PLIST | base64 --decode > /Volumes/workspace/repository/ios/GoogleService-Info.plist
-echo "GoogleService-Info.plist file created"
-
 # export HOMEBREW_NO_INSTALL_CLEANUP=TRUE
 brew install cocoapods
 # have to add node yourself
-brew install node@18.16.0
+brew install node@18
 # link it to the path
-brew link node@18.16.0
+brew link node@18
 
 # Clear npm cache
 npm cache clean --force
@@ -20,3 +17,6 @@ pod deintegrate
 pod install --repo-update
 # the sed command from RN cant find the file... so we have to run it ourselves
 # sed -i -e  $'s/ && (__IPHONE_OS_VERSION_MIN_REQUIRED < __IPHONE_10_0)//' /Volumes/workspace/repository/ios/Pods/RCT-Folly/folly/portability/Time.h
+
+echo $GOOGLE_SERVICE_INFO_PLIST | base64 --decode > /Volumes/workspace/repository/ios/GoogleService-Info.plist
+echo "GoogleService-Info.plist file created"
