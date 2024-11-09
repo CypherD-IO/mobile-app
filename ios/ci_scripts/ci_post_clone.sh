b6c141001f68e416344b3b26f340132bd17199a9
mohanramCypherD <mohanram@cypherd.io>
2024-11-09 22:53:54 +0530

    moved install sentry cli to last

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -17,3 +17,15 @@ pod deintegrate
 pod install --repo-update
 # the sed command from RN cant find the file... so we have to run it ourselves
 # sed -i -e  $'s/ && (__IPHONE_OS_VERSION_MIN_REQUIRED < __IPHONE_10_0)//' /Volumes/workspace/repository/ios/Pods/RCT-Folly/folly/portability/Time.h
+
+export SENTRY_AUTH_TOKEN=$SENTRY_AUTH_TOKEN
+sentry-cli upload-dsym --auth-token $SENTRY_AUTH_TOKEN
+
+echo $GOOGLE_SERVICE_INFO_PLIST | base64 --decode > /Volumes/workspace/repository/ios/GoogleService-Info.plist
+echo "GoogleService-Info.plist file created"
+
+# Create sentry.properties file
+echo $SENTRY_PROPERTIES | base64 --decode > /Volumes/workspace/repository/ios/sentry.properties
+echo "sentry.properties file created"
+
+brew install getsentry/tools/sentry-cli
--- ios/ci_scripts/ci_pre_xcodebuild.sh
+++ ios/ci_scripts/ci_pre_xcodebuild.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-brew install getsentry/tools/sentry-cli
+# brew install getsentry/tools/sentry-cli
 
-export SENTRY_AUTH_TOKEN=$SENTRY_AUTH_TOKEN
-sentry-cli upload-dsym --auth-token $SENTRY_AUTH_TOKEN
+# export SENTRY_AUTH_TOKEN=$SENTRY_AUTH_TOKEN
+# sentry-cli upload-dsym --auth-token $SENTRY_AUTH_TOKEN
 
-echo $GOOGLE_SERVICE_INFO_PLIST | base64 --decode > /Volumes/workspace/repository/ios/GoogleService-Info.plist
-echo "GoogleService-Info.plist file created"
+# echo $GOOGLE_SERVICE_INFO_PLIST | base64 --decode > /Volumes/workspace/repository/ios/GoogleService-Info.plist
+# echo "GoogleService-Info.plist file created"
 
-# Create sentry.properties file
-echo $SENTRY_PROPERTIES | base64 --decode > /Volumes/workspace/repository/ios/sentry.properties
-echo "sentry.properties file created"
+# # Create sentry.properties file
+# echo $SENTRY_PROPERTIES | base64 --decode > /Volumes/workspace/repository/ios/sentry.properties
+# echo "sentry.properties file created"
