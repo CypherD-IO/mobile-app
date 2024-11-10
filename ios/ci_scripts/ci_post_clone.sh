d7700967a31c647d676e7ceb081e649833e3e64a
mohanramCypherD <mohanram@cypherd.io>
2024-11-10 16:51:15 +0530

    sentry installation order changed in pre clone script and node warning solved

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -2,30 +2,35 @@
 
 # export HOMEBREW_NO_INSTALL_CLEANUP=TRUE
 brew install cocoapods
-# have to add node yourself
+
+# Install specific node version
 brew install node@18
-# link it to the path
-brew link node@18
+export PATH="/usr/local/opt/node@18/bin:$PATH"
+export LDFLAGS="-L/usr/local/opt/node@18/lib"
+export CPPFLAGS="-I/usr/local/opt/node@18/include"
+
+# Install specific node version using n
+npm install -g n
+n 18.16.0
+
+# Verify node version
+node -v
 
 # Clear npm cache
 npm cache clean --force
 
-# Install dependencies you manage with CocoaPods.
+# Install dependencies
 npm install --legacy-peer-deps
+
+# Clean up and install pods
 rm -rf /Volumes/workspace/DerivedData
 pod deintegrate
+rm -rf Pods
+rm -rf Podfile.lock
 pod install --repo-update
 # the sed command from RN cant find the file... so we have to run it ourselves
 # sed -i -e  $'s/ && (__IPHONE_OS_VERSION_MIN_REQUIRED < __IPHONE_10_0)//' /Volumes/workspace/repository/ios/Pods/RCT-Folly/folly/portability/Time.h
 
-export SENTRY_AUTH_TOKEN=$SENTRY_AUTH_TOKEN
-sentry-cli upload-dsym --auth-token $SENTRY_AUTH_TOKEN
-
-echo $GOOGLE_SERVICE_INFO_PLIST | base64 --decode > /Volumes/workspace/repository/ios/GoogleService-Info.plist
-echo "GoogleService-Info.plist file created"
-
-# Create sentry.properties file
-echo $SENTRY_PROPERTIES | base64 --decode > /Volumes/workspace/repository/ios/sentry.properties
-echo "sentry.properties file created"
-
-brew install getsentry/tools/sentry-cli
+# Verify the installation
+echo "Node version: $(node -v)"
+echo "NPM version: $(npm -v)"
--- ios/ci_scripts/ci_pre_xcodebuild.sh
+++ ios/ci_scripts/ci_pre_xcodebuild.sh
@@ -1,13 +1,23 @@
 #!/bin/sh
 
-# brew install getsentry/tools/sentry-cli
+# Create required files first
+echo $GOOGLE_SERVICE_INFO_PLIST | base64 --decode > /Volumes/workspace/repository/ios/GoogleService-Info.plist
+echo "GoogleService-Info.plist file created"
 
-# export SENTRY_AUTH_TOKEN=$SENTRY_AUTH_TOKEN
-# sentry-cli upload-dsym --auth-token $SENTRY_AUTH_TOKEN
+# Create sentry.properties file
+echo $SENTRY_PROPERTIES | base64 --decode > /Volumes/workspace/repository/ios/sentry.properties
+echo "sentry.properties file created"
 
-# echo $GOOGLE_SERVICE_INFO_PLIST | base64 --decode > /Volumes/workspace/repository/ios/GoogleService-Info.plist
-# echo "GoogleService-Info.plist file created"
+# Install and configure Sentry CLI
+brew install getsentry/tools/sentry-cli
 
-# # Create sentry.properties file
-# echo $SENTRY_PROPERTIES | base64 --decode > /Volumes/workspace/repository/ios/sentry.properties
-# echo "sentry.properties file created"
+# Export auth token
+export SENTRY_AUTH_TOKEN=$SENTRY_AUTH_TOKEN
+
+# Look for dSYM files in the default location
+DSYM_PATH="/Volumes/workspace/DerivedData/Build/Products/Release-iphoneos"
+if [ -d "$DSYM_PATH" ]; then
+    sentry-cli upload-dsym --auth-token $SENTRY_AUTH_TOKEN "$DSYM_PATH"
+else
+    echo "Warning: dSYM path not found at $DSYM_PATH"
+fi
