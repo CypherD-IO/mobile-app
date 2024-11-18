b5df8e001b4df2243a31e25f585b3234cf4052b9
mohanramCypherD <mohanram@cypherd.io>
2024-11-18 10:05:40 +0530

    crrashlytics removed

--- ios/ci_scripts/ci_post_xcodebuild.sh
+++ ios/ci_scripts/ci_post_xcodebuild.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
-"${PODS_ROOT}/FirebaseCrashlytics/upload-symbols" \
--gsp "${PROJECT_DIR}/GoogleService-Info.plist" \
--p ios "${DWARF_DSYM_FOLDER_PATH}/${DWARF_DSYM_FILE_NAME}"
+# #!/bin/sh
+# "${PODS_ROOT}/FirebaseCrashlytics/upload-symbols" \
+# -gsp "${PROJECT_DIR}/GoogleService-Info.plist" \
+# -p ios "${DWARF_DSYM_FOLDER_PATH}/${DWARF_DSYM_FILE_NAME}"
