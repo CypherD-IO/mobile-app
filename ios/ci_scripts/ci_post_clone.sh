28f145886e77fe07f7b8abedcd1c15168c96f2f6
mohanramCypherD <mohanram@cypherd.io>
2024-11-25 22:44:36 +0530

    node version update

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -6,7 +6,7 @@ export HOMEBREW_NO_INSTALL_CLEANUP=TRUE
 # Install CocoaPods using Homebrew
 brew install cocoapods
 
-# Install Node.js version 18.17.0 using nvm (Node Version Manager)
+# Install Node.js version 18.17.1 using nvm (Node Version Manager)
 # Install nvm if not already installed
 if [ ! -d "$HOME/.nvm" ]; then
     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.4/install.sh | bash
@@ -14,13 +14,13 @@ if [ ! -d "$HOME/.nvm" ]; then
     [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 fi
 
-# Load nvm and install Node.js 18.17.0
+# Load nvm and install Node.js 18.17.1
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 
-nvm install 18.17.0
-nvm use 18.17.0
-nvm alias default 18.17.0
+nvm install 18.17.1
+nvm use 18.17.1
+nvm alias default 18.17.1
 
 # Verify Node.js version
 node -v
