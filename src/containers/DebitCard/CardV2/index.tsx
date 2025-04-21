0445e27b0edfbbee0788b561532925d620864a8e
shan-c <shanthan@cypherd.io>
2025-04-21 15:12:27 -0700

    changed card provider to get from card obj for rain erms

--- src/containers/DebitCard/CardV2/index.tsx
+++ src/containers/DebitCard/CardV2/index.tsx
@@ -210,7 +210,7 @@ export default function CypherCardScreen() {
       ) {
         const rainTerms = await getRainTerms();
         setIsTermsAndConditionsModalVisible(!rainTerms);
-      } else if (cardProvider === CardProviders.REAP_CARD) {
+      } else if (selectedCard?.cardProvider === CardProviders.REAP_CARD) {
         setIsTermsAndConditionsModalVisible(
           !get(data, [cardProvider, 'termsAgreedOn'], 0),
         );
