8ba43424c89317ed6580e2e476cd1303d07fcbbf
mohanramCypherD <mohanram@cypherd.io>
2025-05-22 12:56:39 -0500

    fix: notify users on metal card delay

--- src/components/selectPlanModal.tsx
+++ src/components/selectPlanModal.tsx
@@ -221,6 +221,7 @@ export default function SelectPlanModal({
           '/v1/cards/rc/plan/deduct',
           {
             planId: newlyOptedPlan,
+            forgoMetalCard: true,
           },
         );
         const resp = await getWalletProfile(globalState.token);
@@ -732,6 +733,15 @@ export default function SelectPlanModal({
                 </CyDText>
               </CyDText>
             </CyDTouchView>
+            <CyDText className='px-[12px] text-[14px] my-[8px]'>
+              <CyDText className='font-bold underline'>
+                {t('IMPORTANT')}:
+              </CyDText>{' '}
+              {t('METAL_OUT_OF_STOCK')}
+            </CyDText>
+            <CyDText className='px-[12px] text-[14px] my-[8px]'>
+              {t('YOUR_PREMIUM_BENEFITS_WILL_START_IMMEDIATELY')}
+            </CyDText>
 
             <Button
               title={t('GET_PREMIUM')}
