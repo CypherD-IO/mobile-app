510f9b10e99ff1afa66df10e04865ab6dcd2d111
mohanramCypherD <mohanram@cypherd.io>
2025-06-10 07:31:24 -0500

    feat: Add independent e2e tests for PR validation - Configure GitHub Actions to run e2e tests on PRs to beta branch - Tests run independently and don't block PR merging - Support both iOS and Android testing environments - Update Detox config for CI compatibility - Provide detailed test result reporting

--- .detoxrc.js
+++ .detoxrc.js
@@ -42,7 +42,13 @@ module.exports = {
     emulator: {
       type: 'android.emulator',
       device: {
-        avdName: 'Pixel_3a_API_30_x86',
+        avdName: 'Pixel_6_Pro_API_33',
+      },
+    },
+    'emulator.ci': {
+      type: 'android.emulator',
+      device: {
+        avdName: 'test_emulator_api_33',
       },
     },
   },
@@ -75,6 +81,13 @@ module.exports = {
     'android.emu.release': {
       device: 'emulator',
       app: 'android.release',
+      behavior: {
+        launchArgs: { detoxTestMode: 'YES' },
+      },
+    },
+    'android.ci.debug': {
+      device: 'emulator.ci',
+      app: 'android.debug',
       behavior: {
         launchArgs: { detoxTestMode: 'YES' },
       },
