9281b0002c6ee1b22a9db238d759095f4347e888
rixhy-cyd <rishikhesh@cypherd.io>
2025-09-07 16:43:23 -0500

    app images fix

--- assets/images/appImages.ts
+++ assets/images/appImages.ts
@@ -10,7 +10,6 @@ const S3_BUCKET_URL_DAPPS = 'https://public.cypherd.io/assets/dapps';
 // need dm image
 const EMPTY = require('./emptyImg.png');
 const EMPTY_PERSON = require('./emptyPerson.png');
-const EMPTY_SEARCH = require('./emptySearch.jpg');
 const CYPHERD = require('./cypherD.png');
 const POLYGON = require('./polygon.png');
 const AVALANCHE = require('./avalanche.png');
@@ -768,7 +767,6 @@ const PROFILE_AVATAR = require('./profileAvatar.png');
 export const AppImagesMap = {
   common: {
     EMPTY_PERSON,
-    EMPTY_SEARCH,
     PROFILE_AVATAR,
     CYPR_TOKEN_LOCKED,
     AIRDROP_TOKEN_WIGGLE,
@@ -1093,7 +1091,6 @@ export const AppImagesMap = {
 
 enum AppImages {
   EMPTY_PERSON = 'EMPTY_PERSON',
-  EMPTY_SEARCH = 'EMPTY_SEARCH',
   PROFILE_AVATAR = 'PROFILE_AVATAR',
   CYPR_TOKEN_LOCKED = 'CYPR_TOKEN_LOCKED',
   AIRDROP_TOKEN_WIGGLE = 'AIRDROP_TOKEN_WIGGLE',
