7876b50e41320876b691074eb40d2413f7d145d4
mohanramCypherD <mohanram@cypherd.io>
2026-01-19 19:27:52 +0530

    fix(savecontact): app crash on save contact ater txn

--- src/containers/AddressBook/createContact.tsx
+++ src/containers/AddressBook/createContact.tsx
@@ -69,9 +69,10 @@ export const CreateContact = () => {
   const route = useRoute<RouteProp<{ params: RouteParams }, 'params'>>();
   const editContact = route.params ? route.params.editContact : false;
   const editContactName = route.params ? route.params.editContactName : '';
-  const additionalAddress = route.params
-    ? route.params.additionalAddress
-    : { chain: '', toAddress: '' };
+  const additionalAddress =
+    route.params && route.params.additionalAddress
+      ? route.params.additionalAddress
+      : { chain: '', toAddress: '' };
   const [contactBook, setContactBook] = useState(
     route.params?.contactBook ? route.params?.contactBook : {},
   );
@@ -398,7 +399,7 @@ export const CreateContact = () => {
         tempContactInfo[address] = contactToEdit.addresses[address];
       }
       setEditContactInfo(tempContactInfo);
-      if (additionalAddress) {
+      if (additionalAddress.chain !== '' && additionalAddress.toAddress !== '') {
         const additionalAddressesChain = contactBook[editContactName].addresses[
           additionalAddress.chain
         ]
@@ -419,7 +420,7 @@ export const CreateContact = () => {
   }, [additionalAddress]);
 
   useEffect(() => {
-    if (additionalAddress) {
+    if (additionalAddress.chain !== '' && additionalAddress.toAddress !== '') {
       setCreateContactInfo({
         name: '',
         [additionalAddress.chain]: [additionalAddress.toAddress],
@@ -472,7 +473,7 @@ export const CreateContact = () => {
           showToast(
             `${contactDetails.name} ${t('SAVED_TO_CONTACTS_ALL_SMALL')}`,
           );
-          if (additionalAddress) {
+          if (additionalAddress.chain !== '' && additionalAddress.toAddress !== '') {
             navigation.popToTop();
             navigation.navigate(screenTitle.ACTIVITIES);
           } else {
@@ -579,8 +580,7 @@ export const CreateContact = () => {
             editContact: true,
             editContactName: contact.name,
             contactBook,
-            additionalAddress,
-          });
+            ...(additionalAddress.chain !== '' && additionalAddress.toAddress !== '' ? { additionalAddress } : {}),          });
         }}
         onCancel={() => {
           setChooseContactModalVisible(false);
@@ -680,6 +680,9 @@ export const CreateContact = () => {
                       return (
                         editContact ? editContactInfo : createContactInfo
                       )[detail].map((oneAddress, addressIndex) => {
+                        if (!formProps.values[detail as keyof ContactInfo]) {
+                          return null;
+                        }
                         return (
                           <CyDView
                             className='flex-1 mt-[25px] self-center w-[87%]'
